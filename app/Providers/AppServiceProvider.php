8fbff34260f82a831d7a9702b2d127b8e62b8b2a
webdeveloper510 <developerweb6@gmail.com>
2020-09-29 12:08:44 -0700

    changes

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -29,15 +29,10 @@ class AppServiceProvider extends ServiceProvider
      * @return void
      */
     public function boot()
-<<<<<<< HEAD
+
 	{
 	    Schema::defaultStringLength(191);
 	}
 
-=======
-    {
-        //
-        //Schema::defaultStringLength(191);
-    }
->>>>>>> a96164d4bbad8182ea99c0e52ddfc3177cc99f71
+
 }
