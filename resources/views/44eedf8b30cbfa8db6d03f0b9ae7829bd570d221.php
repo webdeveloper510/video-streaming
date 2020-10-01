b7ee624f05071fd8c4c6a87cb4e832b8cef13a88
webdeveloper510 <developerweb6@gmail.com>
2020-10-01 11:54:49 -0700

    changes

--- resources/views/44eedf8b30cbfa8db6d03f0b9ae7829bd570d221.php
+++ resources/views/44eedf8b30cbfa8db6d03f0b9ae7829bd570d221.php
@@ -60,4 +60,4 @@
         </div>
     </body>
 </html>
-<?php /**PATH C:\xampp\htdocs\video-streaming\vendor\laravel\framework\src\Illuminate\Foundation\Exceptions/views/minimal.blade.php ENDPATH**/ ?>
+<?php /**PATH /home/gv0heoa8vyih/public_html/video-streaming/vendor/laravel/framework/src/Illuminate/Foundation/Exceptions/views/minimal.blade.php ENDPATH**/ ?>
