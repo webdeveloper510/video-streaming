785357e0c8d8f1b7e73c40e42c1ce86b57b6e9df
webdeveloper510 <developerweb6@gmail.com>
2020-10-08 00:00:22 +0530

    changes

--- resources/views/layout/cdn.blade.php
+++ resources/views/layout/cdn.blade.php
@@ -13,7 +13,7 @@
 <meta name="description" content="">
 <meta name="author" Content-Type="video/mp4">
    
-    <link rel="icon" href="{{url('images/fevicon/fevicon.png')}}" type="image/gif" />
+    <link rel="icon" href="{{asset('images/fevicon/fevicon.png')}}" type="image/gif" />
 <!-- bootstrap css -->
 <link rel="stylesheet" href="{{asset('design/bootstrap.min.css')}}">
 <!-- Site css -->
@@ -21,46 +21,46 @@
 
 <link rel="stylesheet" href="{{asset('design/style.css')}}" />
 
-<link rel="stylesheet" href="{{url('design/font-awesome.min.css')}}" />
+<link rel="stylesheet" href="{{asset('design/font-awesome.min.css')}}" />
 <!-- responsive css -->
-<link rel="stylesheet" href="{{url('design/responsive.css')}}" />
-<link rel="stylesheet" href="{{url('design/detailpage.css')}}" />
+<link rel="stylesheet" href="{{asset('design/responsive.css')}}" />
+<link rel="stylesheet" href="{{asset('design/detailpage.css')}}" />
 <!-- colors css -->
-<link rel="stylesheet" href="{{url('design/colors1.css')}}" />
+<link rel="stylesheet" href="{{asset('design/colors1.css')}}" />
 <!-- custom css -->
-<link rel="stylesheet" href="{{url('design/custom.css')}}" />
+<link rel="stylesheet" href="{{asset('design/custom.css')}}" />
 <!-- wow Animation css -->
-<link rel="stylesheet" href="{{url('design/animate.css')}}" />
-<link rel="stylesheet" type="text/css" href="{{url('revolution/css/settings.css')}}" />
-<link rel="stylesheet" type="text/css" href="{{url('revolution/css/layers.css')}}" />
-<link rel="stylesheet" type="text/css" href="{{url('revolution/css/navigation.css')}}" />+
+<link rel="stylesheet" href="{{asset('design/animate.css')}}" />
+<link rel="stylesheet" type="text/css" href="{{asset('revolution/css/settings.css')}}" />
+<link rel="stylesheet" type="text/css" href="{{asset('revolution/css/layers.css')}}" />
+<link rel="stylesheet" type="text/css" href="{{asset('revolution/css/navigation.css')}}" />+
 
 
 <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/owl-carousel/1.3.3/owl.carousel.css" />
 <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/owl-carousel/1.3.3/owl.theme.css" />
 
-<script src="{{url('js/jquery.min.js')}}"></script>
-<script src="{{url('js/bootstrap.min.js')}}"></script>
+<script src="{{asset('js/jquery.min.js')}}"></script>
+<script src="{{asset('js/bootstrap.min.js')}}"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/owl-carousel/1.3.3/owl.carousel.js"></script>
 <!-- menu js -->
-<script src="{{url('js/menumaker.js')}}"></script>
+<script src="{{asset('js/menumaker.js')}}"></script>
 <!-- wow animation -->
-<script src="{{url('js/wow.js')}}"></script>
+<script src="{{asset('js/wow.js')}}"></script>
 <!-- custom js -->
-<script src="{{url('js/custom.js')}}"></script>
+<script src="{{asset('js/custom.js')}}"></script>
 <!-- revolution js files -->
-<script src="{{url('revolution/js/jquery.themepunch.tools.min.js')}}"></script>
-<script src="{{url('revolution/js/jquery.themepunch.revolution.min.js')}}"></script>
-<script src="{{url('revolution/js/extensions/revolution.extension.actions.min.js')}}"></script>
-<script src="{{url('revolution/js/extensions/revolution.extension.carousel.min.js')}}"></script>
-<script src="{{url('revolution/js/extensions/revolution.extension.kenburn.min.js')}}"></script>
-<script src="{{url('revolution/js/extensions/revolution.extension.layeranimation.min.js')}}"></script>
-<script src="{{url('revolution/js/extensions/revolution.extension.migration.min.js')}}"></script>
-<script src="{{url('revolution/js/extensions/revolution.extension.navigation.min.js')}}"></script>
-<script src="{{url('revolution/js/extensions/revolution.extension.parallax.min.js')}}"></script>
-<script src="{{url('revolution/js/extensions/revolution.extension.slideanims.min.js')}}"></script>
+<script src="{{asset('revolution/js/jquery.themepunch.tools.min.js')}}"></script>
+<script src="{{asset('revolution/js/jquery.themepunch.revolution.min.js')}}"></script>
+<script src="{{asset('revolution/js/extensions/revolution.extension.actions.min.js')}}"></script>
+<script src="{{asset('revolution/js/extensions/revolution.extension.carousel.min.js')}}"></script>
+<script src="{{asset('revolution/js/extensions/revolution.extension.kenburn.min.js')}}"></script>
+<script src="{{asset('revolution/js/extensions/revolution.extension.layeranimation.min.js')}}"></script>
+<script src="{{asset('revolution/js/extensions/revolution.extension.migration.min.js')}}"></script>
+<script src="{{asset('revolution/js/extensions/revolution.extension.navigation.min.js')}}"></script>
+<script src="{{asset('revolution/js/extensions/revolution.extension.parallax.min.js')}}"></script>
+<script src="{{asset('revolution/js/extensions/revolution.extension.slideanims.min.js')}}"></script>
 <script src="https://designmodo.com/demo/dropdown-menu-search/prefixfree-1.0.7.js"></script>
-<script src="{{url('revolution/js/extensions/revolution.extension.video.min.js')}}"></script>
+<script src="{{asset('revolution/js/extensions/revolution.extension.video.min.js')}}"></script>
     <!-- Bootstrap CSS -->
     
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
