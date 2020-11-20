0515e75149a87d529135350c1fec3f147660568e
webdeveloper510 <developerweb6@gmail.com>
2020-11-20 22:28:51 +0530

    changes

--- public/js/my.js
+++ public/js/my.js
@@ -125,7 +125,7 @@ function readURL(input) {
 }
 
 $(document).on('click', '#checkPrice', function () {
-	alert('hello');return false;
+	//alert('hello');return false;
 	var token= $('.token').val();
 	//console.log(token);
 	$.ajax({
