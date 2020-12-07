65c613cdd8300de4633d42f47fc3d3b8e7cec68f
webdeveloper510 <developerweb6@gmail.com>
2020-12-07 23:25:07 +0530

    changes

--- resources/views/7993f9d731bc11673897eed03878929e0caa89ae.php
+++ resources/views/7993f9d731bc11673897eed03878929e0caa89ae.php
@@ -1,6 +1,6 @@
 
 <?php echo $__env->make('layouts.header', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
-
+ if
 <!-- end header -->
 <div class="inner-page">
   <div class="container">
--- resources/views/a1c777865266709af234e74bd08dd1da633e4083.php
+++ resources/views/a1c777865266709af234e74bd08dd1da633e4083.php
@@ -24,7 +24,7 @@
                     <li class="link_click"><a data-toggle="tab" href="#menu4">Artists</a></li>
                     <li  class="link_click" ><a data-toggle="tab" href="#menu2">Offers</a></li>
                     </ul>
-
+                      <?php if($login): ?>
                     <div class="tab-content">
                     <div id="home" class="tab-pane fade1 in active">
                         <h3 style="color: #fff;">Video</h3>
@@ -476,6 +476,7 @@
                     
                      </div>
                     </div>
+                    <?php endif; ?>
                 </ul>
             
               <li id="search">
