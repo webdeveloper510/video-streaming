99125106d42147e1f29a9d0dbf3813de94f10a06
webdeveloper510 <developerweb6@gmail.com>
2020-12-12 04:26:10 +0530

    changes

--- resources/views/00b8b9752831bb01fb6a187162c97b61a26db069.php
+++ resources/views/00b8b9752831bb01fb6a187162c97b61a26db069.php
@@ -55,7 +55,9 @@
           </div> 
 
             
-
+<button type="button" class="btn btn-primary" style="float:right;" data-bs-toggle="modal" data-bs-target="#exampleModal">
+  Forget Password
+</button>
 <!-- if there are login errors, show them here -->
 <p>
   
@@ -68,20 +70,43 @@
 
             
              
-          </div>
-              <?php endif; ?>
+      </div>
+          <?php endif; ?>
 
-            <p><?php echo e(Form::submit('Login!',['class'=>'btn btn-primary'])); ?></p>
+            <p class="pt-3"><?php echo e(Form::submit('Login!',['class'=>'btn btn-primary'])); ?></p>
             <?php echo e(Form::close()); ?>
 
            
-        
+    <div class="bottom mt-5">
 <p class="text-white">Don't have an account yet ?</p>
 <a href="<?php echo e(URL::to('register')); ?>" class="ffff ">Signup Now</a>
+</div>
+
         </div>
       </div>
     </div>
   </section>
+  <!-- Button trigger modal -->
+
+
+<!-- Modal -->
+<div class="modal" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Reset Password</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        <input type="email" id="email"/> 
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary" id="forgetLink">Send Reset Link</button>
+      </div>
+    </div>
+  </div>
+</div>
   <style>
 
     a.ffff {
@@ -90,6 +115,7 @@
 
   </style>
   <?php echo $__env->make('layouts.footer', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
-
+<!-- JavaScript Bundle with Popper -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
 <!--   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script> -->
   <?php /**PATH C:\xampp\htdocs\video-streaming\resources\views/login.blade.php ENDPATH**/ ?>
