f27fbfe215deffe699cfeaf763abd6fa1d1cf654
webdeveloper510 <developerweb6@gmail.com>
2020-12-18 00:27:41 +0530

    changes

---
+++ resources/views/70e8d7c4a11ea11f04c3f85c986cc35253e7702e.php
@@ -0,0 +1,218 @@
+<?php echo $__env->make('layouts.header', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
+
+<div class="row">
+    <div class="col-md-12 col-sm-12 col-lg-12">
+        <div class="coverimg">
+            <img src="http://localhost/video-streaming/storage/app/public/uploads/1606412754_marriage.jpg" width="100%" height="300px">
+        </div>
+        <div class="profileimg">
+        <img src="http://localhost/video-streaming/storage/app/public/uploads/1606412754_marriage.jpg" width="200px" height="200px">
+        </div>
+        <div class="artistdetail11 mb-5">
+            <h3>Artist Name <i class="fa fa-star" style="color:red;"></i></h3>
+         
+            <button type="button" class="btn btn-primary">Add to Library</button>
+          </div>
+          <div class="artistlist11">
+           <div class="row text-center">
+            <div class="col">
+                  <h3>Offers</h3>
+              </div>
+               <div class="col">
+                  <h3>Profile</h3>
+              </div>
+               <div class="col">
+                  <h3>All Content</h3>
+              </div>
+               <div class="col">
+                  <h3>Playlists</h3>
+              </div>
+               <div class="col">
+                  <h3>Audio</h3>
+              </div>
+              </div>
+
+          </div>
+      <div class="container">
+                 <!-- --------------Simples Videos -------------------->
+              <h2>Simples</h2>
+              <div class="text-right seemore11">
+              <button type="button" class="btn btn-primary">See More</button>
+          </div>
+          <div class="row mb-5">
+              
+            <div class="col-md-3">
+            <video width="100%" height="100%" controls>
+                <source src="movie.mp4" type="video/mp4">
+                <source src="movie.ogg" type="video/ogg">
+                Your browser does not support the video tag.
+            </video>
+            </div>
+            <div class="col-md-3">
+            <video width="100%" height="100%" controls>
+                <source src="movie.mp4" type="video/mp4">
+                <source src="movie.ogg" type="video/ogg">
+                Your browser does not support the video tag.
+            </video>
+            </div>
+            <div class="col-md-3">
+            <video width="100%" height="100%" controls>
+                <source src="movie.mp4" type="video/mp4">
+                <source src="movie.ogg" type="video/ogg">
+                Your browser does not support the video tag.
+            </video>
+            </div>
+            <div class="col-md-3">
+            <video width="100%" height="100%" controls>
+                <source src="movie.mp4" type="video/mp4">
+                <source src="movie.ogg" type="video/ogg">
+                Your browser does not support the video tag.
+            </video>
+            </div>
+          </div>
+
+
+<!-- -------------------Offer Videos ----------------------------->
+              <h2><i class="fa fa-shopping-cart" style="color:black"></i>  Offer</h2>
+              <div class="text-right seemore11">
+              <button type="button" class="btn btn-primary">See More</button>
+          </div>
+          <div class="row mb-5">
+             
+            <div class="col-md-3">
+            <video width="100%" height="100%" controls>
+                <source src="movie.mp4" type="video/mp4">
+                <source src="movie.ogg" type="video/ogg">
+                Your browser does not support the video tag.
+            </video>
+            </div>
+            <div class="col-md-3">
+            <video width="100%" height="100%" controls>
+                <source src="movie.mp4" type="video/mp4">
+                <source src="movie.ogg" type="video/ogg">
+                Your browser does not support the video tag.
+            </video>
+            </div>
+          
+          </div>
+
+                 <!-- --------------Clubs videos -------------------->
+            <h2><i class="fa fa-rocket"></i>  Clubs</h2>
+            <div class="text-right seemore11">
+              <button type="button" class="btn btn-primary">See More</button>
+          </div>
+          <div class="row mb-5">
+             
+            <div class="col-md-3">
+            <video width="100%" height="100%" controls>
+                <source src="movie.mp4" type="video/mp4">
+                <source src="movie.ogg" type="video/ogg">
+                Your browser does not support the video tag.
+            </video>
+            </div>
+            <div class="col-md-3">
+            <video width="100%" height="100%" controls>
+                <source src="movie.mp4" type="video/mp4">
+                <source src="movie.ogg" type="video/ogg">
+                Your browser does not support the video tag.
+            </video>
+            </div>
+            <div class="col-md-3">
+            <video width="100%" height="100%" controls>
+                <source src="movie.mp4" type="video/mp4">
+                <source src="movie.ogg" type="video/ogg">
+                Your browser does not support the video tag.
+            </video>
+               </div>
+            <div class="col-md-3">
+            <video width="100%" height="100%" controls>
+                <source src="movie.mp4" type="video/mp4">
+                <source src="movie.ogg" type="video/ogg">
+                Your browser does not support the video tag.
+            </video>
+            </div>
+          </div>
+
+           <!-- --------------Audio videos -------------------->
+                <h2><i class="fa fa-microphone"></i> Recodings</h2>
+                <div class="text-right seemore11">
+              <button type="button" class="btn btn-primary">See More</button>
+          </div>
+          <div class="row mb-5">
+             
+           
+            <div class="col-md-3">
+           <audio controls>
+              <source src="horse.ogg" type="audio/ogg">
+              <source src="horse.mp3" type="audio/mpeg">
+              Your browser does not support the audio tag.
+            </audio>
+            </div>
+            <div class="col-md-3">
+           <audio controls>
+              
+              <source src="horse.mp3" type="audio/mpeg">
+              Your browser does not support the audio tag.
+            </audio>
+            </div>
+            <div class="col-md-3">
+           <audio controls>
+            
+              <source src="horse.mp3" type="audio/mpeg">
+              Your browser does not support the audio tag.
+            </audio>
+               </div>
+            <div class="col-md-3">
+           <audio controls>
+            
+              <source src="horse.mp3" type="audio/mpeg">
+              Your browser does not support the audio tag.
+            </audio>
+            </div>
+          </div>
+
+            <!-- --------------Goals videos -------------------->
+              <h2><i class="fa fa-video-camera"></i> Long Videos</h2>
+              <div class="text-right seemore11">
+              <button type="button" class="btn btn-primary">See More</button>
+          </div>
+          <div class="row mb-5">
+            
+            <div class="col-md-3">
+            <video width="100%" height="100%" controls>
+                <source src="movie.mp4" type="video/mp4">
+                <source src="movie.ogg" type="video/ogg">
+                Your browser does not support the video tag.
+            </video>
+            </div>
+            <div class="col-md-3">
+            <video width="100%" height="100%" controls>
+                <source src="movie.mp4" type="video/mp4">
+                <source src="movie.ogg" type="video/ogg">
+                Your browser does not support the video tag.
+            </video>
+            </div>
+            <div class="col-md-3">
+            <video width="100%" height="100%" controls>
+                <source src="movie.mp4" type="video/mp4">
+                <source src="movie.ogg" type="video/ogg">
+                Your browser does not support the video tag.
+            </video>
+               </div>
+            <div class="col-md-3">
+            <video width="100%" height="100%" controls>
+                <source src="movie.mp4" type="video/mp4">
+                <source src="movie.ogg" type="video/ogg">
+                Your browser does not support the video tag.
+            </video>
+            </div>
+          </div>
+
+          
+       
+    </div>
+</div>
+</div>
+
+<?php echo $__env->make('layouts.footer', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
+<?php /**PATH C:\xampp\htdocs\video-streaming\resources\views/artistselling.blade.php ENDPATH**/ ?>
---
+++ resources/views/fea2842704fa240e2dc42c2646b96f232b392274.php
@@ -0,0 +1,99 @@
+
+<!doctype html>
+<html lang="en">
+  <head>
+   
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="<?php echo e(asset('design/artist.css')); ?>" />
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+    <style>
+  form.form-inline .form-control {
+    width: 60% !important;
+    background-color: #ffffff !important;
+    color: black !important;
+}
+      button.btn.btn-primary.my-2.my-sm-0 {
+          margin-left: -11px;
+      }
+
+      .artist .profileImage {
+    width: 125px;
+    height: 125px;
+    border-radius: 50%;
+    background: #512DA8;
+    font-size: 75px;
+    color: #fff;
+    text-align: center;
+    line-height: 116px;
+    margin-right: 14px;
+    margin-top: 4px;
+}
+
+    </style>
+  </head>
+  <body>
+    <?php echo $__env->make('layouts.header', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
+   <div class="container">
+       <div class="row">
+           <div class="col-md-4">
+            <div class="form-group mt-4">
+               
+              <form class="form-inline text-center align-center">
+                <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+                <button class="btn btn-primary my-2 my-sm-0" type="submit"><i class="fa fa-search"></i></button>
+              </form>
+             
+              
+         </div>
+           </div>
+           <div class="col-md-4 text-right pt-3 mt-3">
+              <span>Short:</span>
+           </div>
+           <div class="col-md-4 mt-3">
+  
+            <select class="custom-select form-inline m-2">
+             
+                 <option selected>Open this select menu</option>
+                 <option value="1">One</option>
+                 <option value="2">Two</option>
+                 <option value="3">Three</option>
+               </select>
+           </div>
+       </div>
+       <hr>
+       <div class="row mb-5">
+    <?php $__currentLoopData = $artists; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $artist): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+           <div class="col-md-2">
+             
+               <div class="artist text-center">
+               <?php if($artist->profilepicture): ?>
+                <img src="<?php echo e(url('storage/app/public/uploads/'.$artist->profilepicture)); ?>">
+                <div class="overlay">
+                  <a href="<?php echo e(url('artistDetail/'.$artist->id)); ?>"><?php echo e($artist->nickname); ?></a>
+               </div>
+               <?php else: ?>
+               <a href="<?php echo e(url('artistDetail/'.$artist->id)); ?>">
+		    	  <span class="firstName" style="display: none;"><?php echo e($artist->nickname); ?></span>
+	           	<div class="profileImage"></div>
+
+               </a>
+             
+             <?php endif; ?>
+               </div>
+           </div>
+             <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+
+       </div>
+
+    <div class="pagination"><?php echo e($artists->links()); ?></div>
+
+   </div>
+
+  </body>
+   <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <!--script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script-->
+</html><?php /**PATH C:\xampp\htdocs\video-streaming\resources\views/artists.blade.php ENDPATH**/ ?>
