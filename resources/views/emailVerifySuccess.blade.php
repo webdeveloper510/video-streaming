96bb59d8d0daaf4859b7f29542949e1b3cb11e8d
webdeveloper510 <developerweb6@gmail.com>
2021-01-01 01:30:26 +0530

    changes

--- resources/views/emailVerifySuccess.blade.php
+++ resources/views/emailVerifySuccess.blade.php
@@ -7,7 +7,7 @@
         <div class="successs p-3 text-center">
         	<img src="https://images.vexels.com/media/users/3/157931/isolated/preview/604a0cadf94914c7ee6c6e552e9b4487-curved-check-mark-circle-icon-by-vexels.png" width="300px">
         	<h1>Thank You! </h1>
-            <h3> Email Verify Please click Log in </h3>
+            <h3> Email Verified Please click Log in </h3>
 
             <a href="{{url('/login')}}"><button type="button" class="btn btn-primary">Login</button></a>
         </div>
