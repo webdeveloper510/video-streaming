30c36635531295b38cf596c73ceaf2ba6ed7ec1f
webdeveloper510 <developerweb6@gmail.com>
2021-01-05 22:56:21 +0530

    changes

--- resources/views/initial.blade.php
+++ resources/views/initial.blade.php
@@ -134,7 +134,7 @@
 	</div>
     <!--Carousel Wrapper-->
     @if($login)
-    <div id="recently_search" class="carousel slide carousel-multi-item" data-ride="carousel">
+    <div id="recently_search" class="row">
 
       @forelse ($recently as $recnt)
             @if($recnt->type=='video')
