50ce0f7ab2188e3c82788c72969c31e26b84d081
webdeveloper510 <developerweb6@gmail.com>
2021-01-06 20:55:33 +0000

    changes

--- resources/views/1249da2aa080552ce91830a7ab9b15aec21e908b.php
+++ resources/views/1249da2aa080552ce91830a7ab9b15aec21e908b.php
@@ -1,45 +1,522 @@
 <?php echo $__env->make('artists.dashboard', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
-      <section class="background1 pt-5">
+
+
+<div class="row">
+    <div class="col-md-12 col-sm-12 col-lg-12">
+        <div class="coverimg">
+            <img src="http://localhost/video-streaming/storage/app/public/uploads/1606412754_marriage.jpg" width="100%" height="300px">
+        </div>
+        <div class="profileimg">
+        <img src="http://localhost/video-streaming/storage/app/public/uploads/1606412754_marriage.jpg" width="200px" height="200px">
+        </div>
+        <div class="artistdetail11 mb-5">
+            <h3>Artist Name <i class="fa fa-star" style="color:red;"></i></h3>
+
+          </div>
+          <nav>
+  <div class="nav nav-tabs" id="nav-tab" role="tablist">
+    <a class="nav-link tabss " id="nav-home-tab" data-toggle="tab" href="#nav-home" role="tab" aria-controls="nav-home" aria-selected="true">Offers</a>
+    <a class="nav-link tabss" id="nav-profile-tab" data-toggle="tab" href="#nav-profile" role="tab" aria-controls="nav-profile" aria-selected="false">Profile</a>
+    <a class="nav-link tabss active" id="nav-contact-tab" data-toggle="tab" href="#nav-contact" role="tab" aria-controls="nav-contact" aria-selected="false">Collection</a>
    
-    <div class="row">
-      <div class="col"></div>
-      <div class="col-md-5 mt-5">
-        <div class="overlay1 mt-5 ">
-           <div class="text-center p-3">
-    
-   <img width="150px"; height="100px" src="<?php echo e(url('storage/app/public/uploads/1602525455_etsy inspo.jpg')); ?>">
+  </div>
+</nav>
+<div class="tab-content" id="nav-tabContent">
 
- </div>
-     <table class="text-white table">
-      <thead>
+     <!-- --------------Offer videos -------------------->
+
+  <div class="tab-pane fade " id="nav-home" role="tabpanel" aria-labelledby="nav-home-tab"> 
+  <!-- -------------------Offer Videos ----------------------------->
   
-          </thead>
-          <tbody>
-          <tr class="text-white">
-            <th> Nick Name:-</th>
-            <td><?php echo e($contentUser->nickname); ?></td>
-          </tr>
-          <tr class="text-white">
-            <th>Email Id:-</th>
-            <td><?php echo e($contentUser->email); ?></td>
-          </tr>
-          <tr class="text-white">
-            <th>Model:-</th>
-            <td></td>
-          </tr>
-      </tbody>
-
-     </table>
-
+   <h2> Offers</h2>
+              
+          <div class="container">
+   <div class="row mb-5">
+     
+      
+    <div class="col-md-12">
+      <div class="artistoffer row">
+        <div class="col-md-2 pt-3 pl-3">
+        <img src="http://localhost/video-streaming/storage/app/public/uploads/1606412754_marriage.jpg" width="100px" height="100px">
+    </div>
+        <div class="col-md-8 pl-5">
+         <h2><a href="">Offer Title</a></h2>
+         <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries,</p>
+           <a href="">Arisit Name</a>
+           <br>
+         <a href="">Categories :</a>
+          
+        </div>
+        <div class="col-md-2">
+         <h4>10/min PAZ</h4>
+        </div>
+      </div>
+    </div>
+        <hr>
+    <div class="col-md-12">
+      <div class="artistoffer row">
+        <div class="col-md-2">
+        <img src="http://localhost/video-streaming/storage/app/public/uploads/1606412754_marriage.jpg" width="100px" height="100px">
+    </div>
+        <div class="col-md-8 pl-5">
+        <h2><a href="">Offer Title</a></h2>
+         <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries,</p>
+           <a href="">Arisit Name</a>
+           <br>
+         <a href="">Categories :</a>
+          
+        </div>
+        <div class="col-md-2">
+        <h4>10/min PAZ</h4>
+        </div>
+      </div>
+    </div>
+    <hr>
+    <div class="col-md-12">
+      <div class="artistoffer row">
+        <div class="col-md-2">
+        <img src="http://localhost/video-streaming/storage/app/public/uploads/1606412754_marriage.jpg" width="100px" height="100px">
+    </div>
+        <div class="col-md-8 pl-5">
+        <h2><a href="">Offer Title</a></h2>
+         <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries,</p>
+           <a href="">Arisit Name</a>
+           <br>
+         <a href="">Categories :</a>
+         
+        </div>
+        <div class="col-md-2">
+        <h4>10/min PAZ</h4>
+        </div>
+      </div>
+    </div>
+    <hr>
+    <div class="col-md-12">
+      <div class="artistoffer row">
+        <div class="col-md-2">
+        <img src="http://localhost/video-streaming/storage/app/public/uploads/1606412754_marriage.jpg" width="100px" height="100px">
+    </div>
+        <div class="col-md-8 pl-5">
+         <h2><a href="">Offer Title</a></h2>
+         <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries,</p>
+           <a href="">Arisit Name</a>
+           <br>
+         <a href="">Categories :</a>
+        
+        </div>
+        <div class="col-md-2">
+        <h4>10/min PAZ</h4>
+        </div>
+      </div>
+    </div>
+    <hr>
+    <div class="col-md-12">
+      <div class="artistoffer row">
+        <div class="col-md-2">
+        <img src="http://localhost/video-streaming/storage/app/public/uploads/1606412754_marriage.jpg" width="100px" height="100px">
+    </div>
+        <div class="col-md-8 pl-5">
+         <h2><a href="">Offer Title</a></h2>
+         <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries,</p>
+           <a href="">Arisit Name</a>
+           <br>
+         <a href="">Categories :</a>
+         
+        </div>
+        <div class="col-md-2">
+        <h4>10/min PAZ</h4>
+        </div>
+      </div>
+    </div>
+    <hr>
+    <div class="col-md-12">
+      <div class="artistoffer row">
+        <div class="col-md-2">
+        <img src="http://localhost/video-streaming/storage/app/public/uploads/1606412754_marriage.jpg" width="100px" height="100px">
+    </div>
+        <div class="col-md-8 pl-5">
+         <h2>Offer Title</h2>
+         <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries,</p>
+           <a href="">Arisit Name</a>
+           <br>
+         <a href="">Categories :</a>
+         
+        </div>
+        <div class="col-md-2">
+         <h4>10/min PAZ</h4>
+        </div>
+      </div>
+    </div>
    </div>
- </div>
-<div class="col"></div>
 
-   </div>
+    </div>
+
+    <style type="text/css">
+        .row hr {
+    width: 100%;
+}
+ </style>
+</div>
+
+  <!-- --------------Contant videos -------------------->
+
+  <div class="tab-pane fade show active" id="nav-contact" role="tabpanel" aria-labelledby="nav-contact-tab">           
+
+   <div class="container">
+    <div class="row mb-5">
+        <div class="col"></div>
+        <div class="col"></div>
+        <div class="col-md-4 text-right">
+               <button type="button" class="btn btn-primary">Add to Library</button>
+      <select class="form-select form-control" aria-label="Default select example">
+  <option selected>Choose . . . . . .</option>
+  <option value="1">Audio</option>
+  <option value="2">Video</option>
+  <option value="3">Playlists</option>
+  
+</select>
+</div>
+</div>
+                 <!-- --------------Simples Videos -------------------->
+
+             
+              
+          <div class="row mb-5">
+              
+            <div class="col-md-4 mb-3">
+            <video width="100%" height="100%" controls>
+                <source src="movie.mp4" type="video/mp4">
+                <source src="movie.ogg" type="video/ogg">
+                Your browser does not support the video tag.
+            </video>
+            </div>
+            <div class="col-md-4 mb-3">
+            <video width="100%" height="100%" controls>
+                <source src="movie.mp4" type="video/mp4">
+                <source src="movie.ogg" type="video/ogg">
+                Your browser does not support the video tag.
+            </video>
+            </div>
+            <div class="col-md-4 mb-3">
+            <video width="100%" height="100%" controls>
+                <source src="movie.mp4" type="video/mp4">
+                <source src="movie.ogg" type="video/ogg">
+                Your browser does not support the video tag.
+            </video>
+            </div>
+            
+          </div>
+
+                    <!-- --------------Playlists Videos -------------------->
 
 
-  </section>
-      <!-- End Navbar -->
+         <h2>Playlists</h2>
+          <div class="row mb-5">
+             
+            <div class="col-md-4 mb-3 play1">
+         <div class="overlayplay1">
+           <h2 class="text-white">21</h2>
+                <i class="fa fa-play"></i>
+                    </div>
+            <video width="100%" height="100%" controls>
+                <source src="movie.mp4" type="video/mp4">
+                <source src="movie.ogg" type="video/ogg">
+                Your browser does not support the video tag.
+            </video>
+            
+        
+            </div>
+            <div class="col-md-4 mb-3 play2">
+           
+            <video width="100%" height="100%" controls>
+                <source src="movie.mp4" type="video/mp4">
+                <source src="movie.ogg" type="video/ogg">
+                Your browser does not support the video tag.
+            </video>
+              <div class="overlayplay2">
+                <h2 class="text-white">21</h2>
+                <i class="fa fa-play"></i>
+        </div>
+            </div>
+            <div class="col-md-4 mb-3 play3">
+            
+            <video width="100%" height="100%" controls>
+                <source src="movie.mp4" type="video/mp4">
+                <source src="movie.ogg" type="video/ogg">
+                Your browser does not support the video tag.
+            </video>
+             <div class="overlayplay3">
+                 <h2 class="text-white">21</h2>
+                <i class="fa fa-play"></i>
+        </div>
+            </div>
+            
+          </div>
 
+                 <!-- --------------Clubs videos -------------------->
+        
+        <div class="row mb-5">
+
+
+        <div class="col-md-4 mb-3">
+            <img src="<?php echo e(asset('images/logos/voice.jpg')); ?>">
+        <audio controls>
+
+        <source src="horse.mp3" type="audio/mp3">
+        Your browser does not support the audio tag.
+        </audio>
+        </div>
+        <div class="col-md-4 mb-3">
+            <img src="<?php echo e(asset('images/logos/voice.jpg')); ?>">
+        <audio controls>
+
+        <source src="horse.mp3" type="audio/mpeg">
+        Your browser does not support the audio tag.
+        </audio>
+        </div>
+        <div class="col-md-4 mb-3">
+             <img src="<?php echo e(asset('images/logos/voice.jpg')); ?>">
+        <audio controls>
+
+        <source src="horse.mp3" type="audio/mpeg">
+        Your browser does not support the audio tag.
+        </audio>
+        </div>
+       
+        </div>
+         
+             
+          <div class="row mb-5">
+              
+            <div class="col-md-4 mb-3">
+            <video width="100%" height="100%" controls>
+                <source src="movie.mp4" type="video/mp4">
+                <source src="movie.ogg" type="video/ogg">
+                Your browser does not support the video tag.
+            </video>
+            </div>
+            <div class="col-md-4 mb-3">
+            <video width="100%" height="100%" controls>
+                <source src="movie.mp4" type="video/mp4">
+                <source src="movie.ogg" type="video/ogg">
+                Your browser does not support the video tag.
+            </video>
+            </div>
+            <div class="col-md-4 mb-3">
+            <video width="100%" height="100%" controls>
+                <source src="movie.mp4" type="video/mp4">
+                <source src="movie.ogg" type="video/ogg">
+                Your browser does not support the video tag.
+            </video>
+            </div>
+            
+          </div>
+
+            <!-- --------------Long videos -------------------->
+            
+             
+              
+          <div class="row mb-5">
+            
+            <div class="col-md-4 mb-3">
+            <video width="100%" height="100%" controls>
+                <source src="movie.mp4" type="video/mp4">
+                <source src="movie.ogg" type="video/ogg">
+                Your browser does not support the video tag.
+            </video>
+            </div>
+            <div class="col-md-4 mb-3">
+            <video width="100%" height="100%" controls>
+                <source src="movie.mp4" type="video/mp4">
+                <source src="movie.ogg" type="video/ogg">
+                Your browser does not support the video tag.
+            </video>
+            </div>
+            <div class="col-md-4 mb-3">
+            <video width="100%" height="100%" controls>
+                <source src="movie.mp4" type="video/mp4">
+                <source src="movie.ogg" type="video/ogg">
+                Your browser does not support the video tag.
+            </video>
+               </div>
+           
+          </div>
+
+          
+       
+    </div></div>
+
+
+        <div class="tab-pane fade" id="nav-profile" role="tabpanel" aria-labelledby="nav-profile-tab">
+          
+         
+  <!------------------------------------------ Profile veiw ------------------------------------->
+  
+  <div class="container">
+      <h1>Overview</h1>
+      <div class="row">
+        <div class="col-md-2 col-sm-2 col-lg-2">
+        </div>
+        <div class="col-md-8 col-sm-8 col-lg-8">
+            <video width="100%" height="100%" controls>
+                      <source src="movie.mp4" type="video/mp4">
+                      <source src="movie.ogg" type="video/ogg">
+                      Your browser does not support the video tag.
+                  </video>
+  
+          </div>
+            <div class="col-md-2 col-sm-2 col-lg-2 mb-3">
+            </div>
+              <div class="col-md-12 col-sm-12 col-lg-12 text-center mt-5">
+                <h3>About Me</h3>
+                <hr>
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries.</p>
+              </div>
+              <hr>
+  
+      </div>
+      <div class="row text-center text-black">
+            <div class="col">
+              <label><b>Gender</b></label>
+              <p>Female</p>
+            </div>
+            <div class="col">
+                <label><b>Sexology</b></label>
+                <p>Homo</p>
+            </div>
+            <div class="col">
+              <label><b>Height</b></label>
+              <p>167cm</p>
+            </div>
+            <div class="col">
+                  <label><b>Weight</b></label>
+              <p>Normal</p>
+            </div>
+      </div>
+        <div class="row text-center mb-5">
+            <div class="col">
+                <label><b>Hair Color</b></label>
+              <p>Blue</p>
+            </div>
+            <div class="col">
+                <label><b>Eyes Color</b></label>
+              <p>Green</p>
+            </div>
+            <div class="col">
+                <label><b>Privy part</b></label>
+              <p>Shaved</p>
+            </div>
+            <div class="col">
+                <label><b>Hair Length</b></label>
+              <p>Long</p>
+            </div>
+          </div>
+        </div>
+  
+  </div>
+  </div>
+  
+</div>
+
+    
+    
+</div>
+</div>
+<style>
+ul.nav.nav-tabs li {
+    width: 20% !important;
+}
+ul.nav.nav-tabs li a {
+    font-size: 15px !important;
+    color: white;
+}
+div#nav-tab {
+    background: pink;
+}
+.nav-link.tabss {
+    width: 33.33%;
+    text-align: center;
+    color: white;
+}
+.profileimg img {
+    position: absolute;
+    top: 133px;
+    border: 3px solid white;
+    border-radius: 50%;
+}
+.artistdetail11.mb-5 {
+    margin-left: 14%;
+    padding-top: 11px;
+}
+.col-md-4.mb-3 img {
+    height: 165px;
+    padding-left: 7px;
+    margin-bottom: -23px;
+}
+.col-md-4.mb-3.play1:hover .overlayplay1 {
+  opacity: 1;
+}
+.col-md-4.mb-3.play2:hover .overlayplay2 {
+  opacity: 1;
+}
+.col-md-4.mb-3.play3:hover .overlayplay3 {
+  opacity: 1;
+}
+.overlayplay1 {
+    position: absolute;
+    top: 0;
+    right: 0px;
+    height: 100%;
+    background: rgb(245 243 243 / 51%) !important;
+    color: #f1f1f1;
+    width: 41%;
+    opacity: 0;
+    z-index: 999999999;
+    color: white;
+    font-size: 20px;
+    padding: 20px;
+    text-align: center;
+}
+.col-md-4.text-right .btn.btn-primary {
+    margin-top: -244px;
+}
+.overlayplay2 {
+    position: absolute;
+    top: 0;
+    right: 0px;
+    height: 100%;
+    background: rgb(245 243 243 / 51%) !important;
+    color: #f1f1f1;
+    width: 41%;
+    opacity: 0;
+    z-index: 999999999;
+    color: white;
+    font-size: 20px;
+    padding: 20px;
+    text-align: center;
+}
+
+.overlayplay3 {
+    position: absolute;
+    top: 0;
+    right: 0px;
+    height: 100%;
+    background: rgb(245 243 243 / 51%) !important;
+    color: #f1f1f1;
+    width: 41%;
+    opacity: 0;
+    z-index: 999999999;
+    color: white;
+    font-size: 20px;
+    padding: 20px;
+    text-align: center;
+}
+
+
+</style>
+
+
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
  <?php echo $__env->make('artists.dashboard_footer', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
 <?php /**PATH /home/personalattentio/public_html/developing-streaming/resources/views/artists/profile.blade.php ENDPATH**/ ?>
