9925c752a682cfebf8d230cd5bebf2f767e3c013
webdeveloper510 <developerweb6@gmail.com>
2021-01-08 22:59:48 +0530

    changes

--- resources/views/f9c236b46cecc30c43ce3254f1539382b2df1e4a.php
+++ resources/views/f9c236b46cecc30c43ce3254f1539382b2df1e4a.php
@@ -1,7 +1,7 @@
 </div>
 
 
-
+ <script src="<?php echo e(asset('artistdashboard/js/core/jquery.min.js')); ?>"></script>
  
    <script id="base_url" data-url="<?php echo e(URL::to('/')); ?>" src="<?php echo e(asset('js/my.js')); ?>"></script>
 
@@ -10,9 +10,8 @@
   <!-- Plugin for the momentJs  -->
   <script src="<?php echo e(asset('artistdashboard/js/plugins/moment.min.js')); ?>"></script>
 
-  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
-  <!--  Plugin for Sweet Alert -->
+   Plugin for Sweet Alert -->
   <script src="<?php echo e(asset('artistdashboard/js/plugins/sweetalert2.js')); ?>"></script>
   <!-- Forms Validations Plugin -->
   <script src="<?php echo e(asset('artistdashboard/js/plugins/jquery.validate.min.js')); ?>"></script>
