14b3bde550b01fe3d5a70f4a87f534697e8aa72f
webdeveloper510 <developerweb6@gmail.com>
2021-01-13 21:12:26 +0530

    changes

--- resources/views/getAlldata.blade.php
+++ resources/views/getAlldata.blade.php
@@ -5,7 +5,7 @@
     @if($videos)
    <div class="row pt-5 mt-5">
    @foreach($videos as $vid)
-       <div class="col-md-4">
+       <div class="col-md-4 my-3">
            <video width="350px" height="275px" controls allowfullscreen>
             <source src="{{url('storage/app/public/video/'.$vid->media) }}" type="video/mp4">
             Your browser does not support the video tag.
@@ -15,7 +15,7 @@
     @else
     <div class="row pt-5 mt-5">
    @foreach($audio as $aud)
-       <div class="col-md-4">
+       <div class="col-md-4 my-3">
        <img src="{{asset('images/logos/voice.jpg')}}">
            <audio width="350px" height="275px" controls>
             <source src="{{url('storage/app/public/audio/'.$aud->media) }}" type="audio/mp3">
