ec21b90543833766642bc43cfadaf666a27e9795
webdeveloper510 <developerweb6@gmail.com>
2021-01-18 19:06:25 +0000

    changes

--- resources/views/5d3dff37d82b42bad17e84b928cfadf9c0874520.php
+++ resources/views/5d3dff37d82b42bad17e84b928cfadf9c0874520.php
@@ -531,6 +531,7 @@
 		<a href="<?php echo e(url('/play')); ?>"  class="nav-item nav-link"><i style="font-size: 21px !important;" class="fa fa-play" aria-hidden="true"></i></a>
     
               <a href="<?php echo e(url('/userWithdraw')); ?>" class="nav-item nav-link"><i class="fa fa-money" aria-hidden="true"></i></a>  
+              <a href="<?php echo e(url('/userWithdraw')); ?>" class="nav-item nav-link"><i class="fa fa-newspaper-o"> </i></a>  
             
 
 
