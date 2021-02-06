1d01b7ded80eebedc0fcedfc5dd1e22c9b2f5b4f
webdeveloper510 <developerweb6@gmail.com>
2021-02-06 05:41:01 +0530

    changes

--- resources/views/initial.blade.php
+++ resources/views/initial.blade.php
@@ -286,7 +286,7 @@
                  @if($audio->type=='audio')
               <div class="col-md-4 mb-3 audiohome">
               
-              <img src="{{$audio->audio_pic ? url('storage/app/public/audio/'.$audio->audio_pic): 'https://pornartistzone.com/developing-streaming/public/images/logos/voice.jpg'}}">
+              <img src="{{$audio->audio_pic ? url('storage/app/public/uploads/'.$audio->audio_pic): 'https://pornartistzone.com/developing-streaming/public/images/logos/voice.jpg'}}">
               <audio controls controlsList="nodownload" disablePictureInPicture>
                  <source src="{{url('storage/app/public/audio/'.$audio->media) }}" type="audio/mp3">
             </audio>
