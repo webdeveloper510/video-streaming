37105f04eb4784a5acc1fcb082d707c8d173b8ca
webdeveloper510 <developerweb6@gmail.com>
2021-02-08 21:49:41 +0530

    changes

--- resources/views/25437d7a7e171e95af6c947265080963d2524035.php
+++ resources/views/25437d7a7e171e95af6c947265080963d2524035.php
@@ -5,9 +5,22 @@
 
 <div class="row mt-5 pt-5 text-center">
     <div class="col-md-3">
-    <h3 class="text-center">Due</h3>
+    <!-- <h3 class="text-center">Due</h3> -->
     <div class="columesdashboard">
-    <div class="row">
+    <?php 
+            $_GLOBEL['count'] =0;
+          ?>
+          <?php $__currentLoopData = $count_due_project; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $data): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+              <?php if(date('Y-m-d')== $data->dates): ?>
+                <?php 
+                $_GLOBEL['count']= $_GLOBEL['count']+1;
+                ?>
+              <?php endif; ?>
+          <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+          
+            <h1><?php echo e($_GLOBEL['count']); ?></h1>
+                  <h4>Order/Project <br> Due</h4>
+    <!-- <div class="row">
       <div class="col-6">
       <?php 
             $_GLOBEL['count'] =0;
@@ -40,15 +53,17 @@
                   <h4>Project </h4>
             </div> 
 
-    </div>
+    </div> -->
 
             </div>   
     </div>
          
     <div class="col-md-3">
-    <h3 class="text-center">In Process</h3>
+    <!-- <h3 class="text-center">In Process</h3> -->
     <div class="columesdashboard1">
-    <div class="row">
+    <h1><?php echo e($process_total); ?></h1>
+                  <h4>Order/Project <br> In Process </h4>
+    <!-- <div class="row">
       <div class="col-6">
      
                   <h1><?php echo e($process_total); ?></h1>
@@ -61,14 +76,16 @@
                   <h4>Project </h4>
             </div> 
 
-    </div>
+    </div> -->
 
             </div>   
     </div>
     <div class="col-md-3">
-    <h3 class="text-center">Project</h3>
+    <!-- <h3 class="text-center">Project</h3> -->
     <div class="columesdashboard2">
-    <div class="row">
+    <h1><?php echo e($count_new_projects); ?></h1>
+    <h4 class="text-center">New/Order <br> Project</h4>
+    <!-- <div class="row">
       <div class="col-6">
      
           <h1><?php echo e($count_new_projects); ?></h1>
@@ -81,23 +98,35 @@
                   <h4>Order </h4>
             </div> 
 
-    </div>
+    </div> -->
 
             </div>   
     </div>
    
     <div class="col-md-3">
-    <h3 class="text-center">Collection Items Online</h3>
+   
     <div class="columesdashboard3">
            <h1>345</h1>
-           
+           <h4 class="text-center">Collection Items <br> Online</h4>
         </div>     
     </div>
 </div>
 
 <div class="row">
     <div class="col-md-4">
-             <div class="card" style="width: 18rem;">
+    <div class="card" style="width: 18rem;">
+             <h5 class="card-title text-left pt-3 pl-3">Your Info:</h5>
+             <hr>
+              <div class="card-body">
+                <h5 class="card-title">First Name : artist Name </h5><br>
+                <h5 class="card-title">Country : USA </h5><br>
+                <h5 class="card-title"> Date of Birth : 05.02.1994 </h5><br>
+               
+                <h5 class="card-title">Email : example@gmail.com</h5>
+               
+              </div>
+            </div>
+             <!-- <div class="card" style="width: 18rem;">
              <h5 class="card-title text-left pt-3 pl-3">New Messages:</h5>
              <hr>
               <div class="card-body">
@@ -108,18 +137,32 @@
                 <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.<span class="text-right ml-5 pl-5"><a href="#">Ready</a></span></p>
                
               </div>
-            </div>
+            </div> -->
     </div>
     <div class="col-md-4">
          <div class="card" style="width: 18rem;">
              <h5 class="card-title text-left pt-3 pl-3">Earnings:</h5>
              <hr>
               <div class="card-body text-center">
-                <h3 class="card-title">Today:</h3>
+                <h4 class="card-title">Today:</h4>
                 <h5><?php echo e($today_paz ? $today_paz[0]->tokens:0); ?> PAZ</h5>
-                <h3 class="card-title">This Month:</h3>
+                <h4 class="card-title">This Month:</h4>
                 <h5><?php echo e($month_paz[0]->total_token); ?> PAZ</h5>
-                <h3 class="card-title">This Year:</h3>
+                <h4 class="card-title">This Year:</h4>
+                <h5><?php echo e($year_PAZ[0]->total_token); ?> PAZ</h5>
+              </div>
+            </div>
+    </div>
+    <div class="col-md-4">
+         <div class="card" style="width: 18rem;">
+             <h5 class="card-title text-left pt-3 pl-3">Social Media:</h5>
+             <hr>
+              <div class="card-body text-center">
+                <h4 class="card-title">Today:</h4>
+                <h5><?php echo e($today_paz ? $today_paz[0]->tokens:0); ?> PAZ</h5>
+                <h4 class="card-title">This Month:</h4>
+                <h5><?php echo e($month_paz[0]->total_token); ?> PAZ</h5>
+                <h4 class="card-title">This Year:</h4>
                 <h5><?php echo e($year_PAZ[0]->total_token); ?> PAZ</h5>
               </div>
             </div>
