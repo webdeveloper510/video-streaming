08857e82530f1713f1909169f531c17bf23e14c6
webdeveloper510 <developerweb6@gmail.com>
2021-02-13 00:50:39 +0530

    changes

--- resources/views/artistDetail.blade.php
+++ resources/views/artistDetail.blade.php
@@ -43,7 +43,7 @@
                     <div class="col-md-12">
                     <div class="artistoffer row">
                       <div class="col-md-2">
-                      <video width="100%" height="100%" controls controlsList="nodownload" disablePictureInPicture>
+                      <video width="100%" class="hoverVideo" height="100%" controls controlsList="nodownload" disablePictureInPicture>
                               <source src="{{url('storage/app/public/video/'.$offer->media) }}" type="video/mp4">
                               
                               Your browser does not support the video tag.
@@ -287,7 +287,7 @@ Your browser does not support the audio tag.
         <div class="col-md-2 col-sm-2 col-lg-2">
         </div>
         <div class="col-md-8 col-sm-8 col-lg-8">
-            <video width="100%" height="100%" controls controlsList="nodownload" disablePictureInPicture>
+            <video width="100%" class="hoverVideo" height="100%" controls controlsList="nodownload" disablePictureInPicture>
                       <source src="{{isset($details[0]->media) ? url('storage/app/public/video/'.$details[0]->media) :'https://www.radiantmediaplayer.com/media/big-buck-bunny-360p.mp4' }}" type="video/mp4">
                       Your browser does not support the video tag.
                   </video>
