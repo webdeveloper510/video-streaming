9cbcef9060e1fb173524d45debbfaf6c686921e8
webdeveloper510 <developerweb6@gmail.com>
2021-02-16 04:56:58 +0530

    changes

--- resources/views/initial.blade.php
+++ resources/views/initial.blade.php
@@ -149,11 +149,13 @@
             <source src="{{url('storage/app/public/video/'.$recnt->media) }}" type="video/mp4">
             Your browser does not support the video tag.
           </video>
+          <div class="pricetime">
           <div class="text-left">
           <h6 class="text-white">{{ $recnt->price }}/PAZ</h6>
           </div>
           <div class="text-right">
           <h6 class="text-white">2:00</h6>
+          </div>
           </div>
           <h5>{{ $recnt->title }}</h5>
           </a>
@@ -200,12 +202,14 @@
                   <source src="{{url('storage/app/public/video/'.$pop->media) }}" type="video/mp4">
                   Your browser does not support the video tag.
                 </video>
+                <div class="pricetime">
                 <div class="text-left">
           <h6 class="text-white">{{ $pop->price }}/PAZ</h6>
           </div>
           <div class="text-right">
           <h6 class="text-white">2:00</h6>
           </div>
+</div>
                 <h5>{{ $pop->title }}</h5>
                 </a>
             </div> 
