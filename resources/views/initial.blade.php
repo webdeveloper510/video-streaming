9c8a66841ab5155094b31eebd35bb003bbf647a5
webdeveloper510 <developerweb6@gmail.com>
2021-02-23 00:55:17 +0530

    changes

--- resources/views/initial.blade.php
+++ resources/views/initial.blade.php
@@ -231,7 +231,7 @@
           <h6 class="text-white">{{ $pop->price }}/PAZ</h6>
           </div>
           <div class="text-right">
-          <h6 class="text-white" id="duration_{{$pop->id}}">2:00</h6>
+          <h6 class="text-white" id="duration_{{$pop->id}}">{{ $recnt->duration ? $recnt->duration :'' }}</h6>
             
           <script>
       
