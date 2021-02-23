8a66542b8a64e5a42dd241f038c4142ab4407a45
webdeveloper510 <developerweb6@gmail.com>
2021-02-24 00:23:27 +0530

    changes

--- resources/views/79f1c4606ae45ef93d8b4284cc1a5fc1624975f8.php
+++ resources/views/79f1c4606ae45ef93d8b4284cc1a5fc1624975f8.php
@@ -277,7 +277,7 @@ Your browser does not support the audio tag.
                       Your browser does not support the video tag.
           </video>
           <?php else: ?>
-          <img src="<?php echo e(isset($random[0]->audio_pic) ? url('storage/app/public/uploads/'.$random[0]->audio_pic) : 'http://localhost/laravel/video-streaming/storage/app/public/uploads/1612819644_2020-12-24.jpg'); ?>" width="167px;">
+          <img src="<?php echo e(isset($random[0]->audio_pic) ? url('storage/app/public/uploads/'.$random[0]->audio_pic) : 'https://images.pexels.com/photos/6126313/pexels-photo-6126313.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'); ?>" width="100%;">
           <audio width="100%" height="100%" id="get_duration" controls controlsList="nodownload" disablePictureInPicture>
                <source src="<?php echo e(isset($random[0]->media) ? url('storage/app/public/audio/'.$random[0]->media) :''); ?>" type="audio/mp3">
                      
@@ -462,7 +462,7 @@ Your browser does not support the audio tag.
                 <?php echo e(Form::file('audio_pic',['class'=>'custom-file-input'])); ?>
 
             </div>
-            <input type="hidden" value="<?php echo e($random[0]->id); ?>" name="hid"/>
+            <input type="hidden" value="<?php echo e(isset($random[0]->id)); ?>" name="hid"/>
            
           <div class="col-md-6 mt-2 convert">
            <?php echo e(Form::label('Convert to:', 'Convert to:')); ?> 
