76a00777764ee5ed0323256676c61354f39097f5
webdeveloper510 <developerweb6@gmail.com>
2021-03-02 01:37:14 +0530

    changes

--- public/js/my.js
+++ public/js/my.js
@@ -1950,7 +1950,9 @@ $("#edit").on('click', function () {
     var inf = $(".replace");
     inf.each(function () {
         $(this).replaceWith(function (i, text) {
-
+			 if(this.id=='dob'){
+				return true;
+			 }
             return $("<input>", {
                 type: "text",
                 value: text,
