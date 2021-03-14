b4c210c3f75af55a501fce53b4be3f3f3690092f
webdeveloper510 <developerweb6@gmail.com>
2021-03-14 18:31:15 +0530

    changes

--- app/Registration.php
+++ app/Registration.php
@@ -2098,6 +2098,7 @@ public function getallOffer($flag){
           return  DB::table('offer')
           ->leftjoin('category','offer.categoryid','=','category.id')
           ->select('offer.*','category.category')
+          ->where('offer.offer_status','online')
           ->take(3)
           ->get()
           ->toArray();
