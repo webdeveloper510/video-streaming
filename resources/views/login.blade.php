45637ad69524184e45ddf374c71673eb814e9a3d
webdeveloper510 <developerweb6@gmail.com>
2021-03-15 21:55:21 +0530

    changes

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,8 +1,8 @@
 
-
-<section class="background1">
 @include('layouts.header')
-    <div class="container mt-5 mb-5">
+<section class="background1 ">
+
+    <div class="container pt-5 pb-5">
 
       <div class="row justify-content-center">
         <div class="col-12 col-md-8 col-lg-8 col-xl-6 need_bg text-white mt-5">
