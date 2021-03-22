acee41424497b2bada89a9e6b779a064c2b2246d
webdeveloper510 <developerweb6@gmail.com>
2021-03-23 03:38:13 +0530

    changes

--- resources/views/artists/faq.blade.php
+++ resources/views/artists/faq.blade.php
@@ -84,10 +84,74 @@
                     <ul>
                     <li>PAZ-Messenger</li>
                     <li>PAZ-Feed</li>
-                    <li>Projects (For Freelancing)</li>
+                    <li>Projects</li>
                     <li>PAZ-Live</li>
                     </ul>
                 </div>
+                <h3>Additional links</h3>
+                <ul class="menufooter">
+              <li>
+              <a href="{{url('/terms')}}"><i class="fa fa-angle-right"></i> Terms and conditions</a></li>
+               <li><a href="{{url('/acceptable')}}"><i class="fa fa-angle-right"></i> Acceptable Use Policy</a></li>
+               <li><a href="{{url('/privacy')}}"><i class="fa fa-angle-right"></i> Privacy policy</a></li>
+               <li><a href="{{url('/dmca')}}"><i class="fa fa-angle-right"></i> DMCA Policy</a></li>
+             
+              <li><a href="{{url('/cookie')}}"><i class="fa fa-angle-right"></i> Cookie Policy</a></li>
+            
+              <li><a href="{{url('/disclaimer')}}"><i class="fa fa-angle-right"></i> Disclaimer</a></li>
+              <li><a href="{{url('/userWithdraw')}}"><i class="fa fa-angle-right"></i>  Withdrawal</a></li> 
+              <li><a href="#" data-toggle="modal" data-target="#exampleModal2"><i class="fa fa-angle-right"></i> Contact Us</a></li>
+              <li><a href="{{url('/legal-notice')}}"><i class="fa fa-angle-right"></i>  Legal Notice</a></li> 
+                <!-- Modal -->
+                <div class="modal modal2" id="exampleModal2" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                  <div class="modal-dialog">
+                    <div class="modal-content">
+                      <div class="modal-header ">
+                      {!!Form::open(['id'=>'customer_issue','method' => 'post', 'files'=>true])!!}
+               {{Form::token()}}
+                      <div class="form-group">
+                          <label for="exampleInputEmail1">Email address</label>
+                          <input type="email" name="customer_email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" required>
+                          <small id="emailHelp" class="form-text text-muted">We'll never share your email to the public.</small>
+                        </div>
+                        
+                      <button type="button" class="close footerclose" data-dismiss="modal" aria-label="Close">X</button>
+                      </div>
+                      <div class="modal-body">
+                      <div class="row">
+                        <div class="col"></div>
+                         <div class="col-md-8 my-3">
+                            <div class="text-center">
+                                  <select class="form-select form-control" name="customer_issue" aria-label="Default select example" required>
+                                    <option selected> Select Menu</option>
+                                    <option value="Feature Request">Feature Request</option>
+                                    <option value="Functionality Question">Functionality Question</option>
+                                    <option value="Techincal Issue">Technical Issue</option>
+                                    <option value="General">General</option>
+                                    <option value="Delete Account">Delete Account</option>
+                                    <option value="Other">Other</option>
+                                  </select>
+                              </div>
+                          </div>
+                          <div class="col"></div>
+                          </div>
+
+                        <label>Description</label>
+                        <textarea class="form-control" name="customer_description" placeholder="We would like to read your description..." id="floatingTextarea" required></textarea>
+                        
+                      </div>
+                      <div class="pb-3 pr-3 text-right">
+                      <div class="alert alert-success" style="display:none"></div>
+                      <button class="btn btn-primary" type="submit">Submit</button></div>
+                    
+                    </div>
+
+                    {{Form::close()}}
+                    
+                    </div>
+                  </div>
+                </div>
+            </ul>
             </div>
         </div>
     </div>
@@ -95,6 +159,9 @@
 <style>
 div#accordion .card-header {
     padding: 0;
+}
+ul.menufooter a {
+    color: black;
 }
 </style>
 @include('artists.dashboard_footer')
