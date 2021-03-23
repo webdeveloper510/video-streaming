095aa52fd11bd97046d1ddf88b58cb5e719d48af
webdeveloper510 <developerweb6@gmail.com>
2021-03-24 03:16:39 +0530

    changes

--- resources/views/1460012bc598dc29e9f6ec01b508e61e786bf533.php
+++ resources/views/1460012bc598dc29e9f6ec01b508e61e786bf533.php
@@ -16,7 +16,97 @@
         <ul class="nav custom search">
             <li id="options" onclick="mufunc()">
               <a href="#"><img width="30px" src="<?php echo e(asset('images/logos/filter.png')); ?>"></a></li>
-              <ul class="subnav" style="display: none">
+             
+          
+            <li id="search">
+            <form action="" method="get">
+            
+                <input type="text" name="search_text" id="search_text" placeholder="Search"/>
+
+                <input type="button" name="search_button" id="search_button"></a>
+            </form>
+            </li>
+          </ul>
+        
+        
+        </div>
+         <div class="pl-5"> 
+         <?php if(!$login): ?>
+					  <a href="<?php echo e(url('/register')); ?>" class="nav-item nav-link">Join Free</a>
+              <a href="<?php echo e(url('/login')); ?>" class="nav-item nav-link"> Login</a>  
+           <?php endif; ?> 
+           <?php if($login): ?>
+         <span class="profile-img text-white">
+            <?php echo e($login->nickname); ?>
+
+            <hr/ style="color:white;background: white;">
+            <b><?php echo e($userProfile ? $userProfile[0]->tokens: ''); ?></b>    <b style="font-family: 'Alfa Slab One', cursive;font-weight: 400;">PAZ</b>
+           
+          </span>
+          <?php endif; ?>
+        </div>
+          <br>
+        <a href="<?php echo e(url('/addToken')); ?>"><i class="fa fa-plus text-white" aria-hidden="true"></i> Add Tocken</a>
+       <br>
+        <a href="<?php echo e(url('/play')); ?>"  class="nav-item nav-link"><i style="font-size: 21px !important;" class="fa fa-play" aria-hidden="true"></i> Library</a>
+         <br>
+        <a href="<?php echo e(url('/seeall1/orders')); ?>"  class="nav-item nav-link">
+    <i style="font-size: 21px !important;" class="fa fa-list-alt" aria-hidden="true"></i> Orders
+    <?php if($login && $latestOffer): ?>
+    <div class="noti" style="<?php echo e($latestOffer->userid == $login->id && $latestOffer->is_seen=='no' ? 'display: block' : 'display: none'); ?>">
+    </div>
+    <?php endif; ?>
+    </a>
+    <br>
+        <a href="<?php echo e(url('/logout')); ?>"> <i class="fa fa-power-off" aria-hidden="true"></i> Logout</a>
+
+</div>
+
+
+    <span style="font-size:30px;cursor:pointer" class="togg" onclick="openNav()">&#9776;</span>
+
+    <div class="logomobile text-center">
+    <a href="<?php echo e(url('/')); ?>" class="navbar-brand">
+						<img src="<?php echo e(asset('images/logos/good_quality_logo.png')); ?>" height="50" alt="CoolBrand">
+					</a>
+     </div>
+     <div class="subscrive">
+       <ul>
+       <li class="nav-item">
+              <a class="nav-link text-white" onclick="$('.subss').toggle()" href="<?php echo e($login ? '#' : url('/register')); ?>" ><i class="fa fa-address-card-o"></i></a>
+               
+               
+                <div class="col-md-4 subss" style="display:none;">
+                  <h3>Subscriptions</h3>
+                  <?php $__currentLoopData = $subscribed_artist; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $artist): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+                    <a href="<?php echo e(url('artistDetail/'.$artist->artistid)); ?>">
+                    <div class="row mb-3">
+                      <div class="col">
+                        <img src="<?php echo e(url('storage/app/public/uploads/'.$artist->profilepicture)); ?>" class="img-fluid">
+                        </div>
+                                <div class="col-6 mt-3">
+                                <p><?php echo e($artist->nickname); ?></p>
+                                </div>
+                              <div class="col mt-3">
+                                  <div class="online" style="<?php echo e($artist->by_created==1 ? 'display:block' :'display:none'); ?>">
+                                      </div>
+                              </div>
+                    </div>
+                    </a>
+                    <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+                 
+                                   </div>
+                    </div>
+
+                    
+                </div>
+                
+              </li>
+       </ul>
+     </div>
+</div>
+
+<ul class="subnav" style="display: none">
                 <ul class="nav nav-tabs text-center">
                   <li class="active link_click"><a data-toggle="tab" href="#home">Video</a></li>
                   <li class="link_click"><a data-toggle="tab" href="#menu1">Audio</a></li>
@@ -529,96 +619,6 @@
                   </div>
                  
               </ul>
-          
-            <li id="search">
-            <form action="" method="get">
-            
-                <input type="text" name="search_text" id="search_text" placeholder="Search"/>
-
-                <input type="button" name="search_button" id="search_button"></a>
-            </form>
-            </li>
-          </ul>
-        
-        
-        </div>
-         <div class="pl-5"> 
-         <?php if(!$login): ?>
-					  <a href="<?php echo e(url('/register')); ?>" class="nav-item nav-link">Join Free</a>
-              <a href="<?php echo e(url('/login')); ?>" class="nav-item nav-link"> Login</a>  
-           <?php endif; ?> 
-           <?php if($login): ?>
-         <span class="profile-img text-white">
-            <?php echo e($login->nickname); ?>
-
-            <hr/ style="color:white;background: white;">
-            <b><?php echo e($userProfile ? $userProfile[0]->tokens: ''); ?></b>    <b style="font-family: 'Alfa Slab One', cursive;font-weight: 400;">PAZ</b>
-           
-          </span>
-          <?php endif; ?>
-        </div>
-          <br>
-        <a href="<?php echo e(url('/addToken')); ?>"><i class="fa fa-plus text-white" aria-hidden="true"></i> Add Tocken</a>
-       <br>
-        <a href="<?php echo e(url('/play')); ?>"  class="nav-item nav-link"><i style="font-size: 21px !important;" class="fa fa-play" aria-hidden="true"></i> Library</a>
-         <br>
-        <a href="<?php echo e(url('/seeall1/orders')); ?>"  class="nav-item nav-link">
-    <i style="font-size: 21px !important;" class="fa fa-list-alt" aria-hidden="true"></i> Orders
-    <?php if($login && $latestOffer): ?>
-    <div class="noti" style="<?php echo e($latestOffer->userid == $login->id && $latestOffer->is_seen=='no' ? 'display: block' : 'display: none'); ?>">
-    </div>
-    <?php endif; ?>
-    </a>
-    <br>
-        <a href="<?php echo e(url('/logout')); ?>"> <i class="fa fa-power-off" aria-hidden="true"></i> Logout</a>
-
-</div>
-
-
-    <span style="font-size:30px;cursor:pointer" class="togg" onclick="openNav()">&#9776;</span>
-
-    <div class="logomobile text-center">
-    <a href="<?php echo e(url('/')); ?>" class="navbar-brand">
-						<img src="<?php echo e(asset('images/logos/good_quality_logo.png')); ?>" height="50" alt="CoolBrand">
-					</a>
-     </div>
-     <div class="subscrive">
-       <ul>
-       <li class="nav-item">
-              <a class="nav-link text-white" onclick="$('.subss').toggle()" href="<?php echo e($login ? '#' : url('/register')); ?>" ><i class="fa fa-address-card-o"></i></a>
-               
-               
-                <div class="col-md-4 subss" style="display:none;">
-                  <h3>Subscriptions</h3>
-                  <?php $__currentLoopData = $subscribed_artist; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $artist): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
-                    <a href="<?php echo e(url('artistDetail/'.$artist->artistid)); ?>">
-                    <div class="row mb-3">
-                      <div class="col">
-                        <img src="<?php echo e(url('storage/app/public/uploads/'.$artist->profilepicture)); ?>" class="img-fluid">
-                        </div>
-                                <div class="col-6 mt-3">
-                                <p><?php echo e($artist->nickname); ?></p>
-                                </div>
-                              <div class="col mt-3">
-                                  <div class="online" style="<?php echo e($artist->by_created==1 ? 'display:block' :'display:none'); ?>">
-                                      </div>
-                              </div>
-                    </div>
-                    </a>
-                    <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
-                 
-                                   </div>
-                    </div>
-
-                    
-                </div>
-                
-              </li>
-       </ul>
-     </div>
-</div>
-
-
 
 
   <div class="header_bottom">
@@ -1400,6 +1400,14 @@ li#options {
     padding: 7px 8px 5px 8px;
     height: 35px;
     width: 40px;
+}
+ul.nav.nav-tabs li a {
+    color: white;
+    font-weight: bolder;
+}
+.subnav{
+  width:100%;
+  z-index: 99;
 }
 ul.nav.custom.search img {
     margin-top: -12px;
