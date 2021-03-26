795741493eb9e9dd9684c35c66b64a20014f4386
webdeveloper510 <developerweb6@gmail.com>
2021-03-27 01:52:23 +0530

    changes

--- resources/views/messagePopup.blade.php
+++ resources/views/messagePopup.blade.php
@@ -3,8 +3,8 @@
 <!--Creates the popup content-->
  <div class="popup-content">
  <p>You have not enough PAZ Token!</p>
-			<label>Add PAZ Token :</label>
-			{{Form::text('token', '',['class'=>'form-control token','placeholder'=>'Add Token'])}}
+			<label class="text-left">Add PAZ Token :</label>
+			{{Form::text('token', '',['class'=>'form-control token ','placeholder'=>'Add Token'])}}
 
 				<strong class="bonusPAZ" style="color:black; font-size:14px"></strong>
 
@@ -34,7 +34,15 @@ visibility:visible;
 .popup-content {
 visibility:visible;
 }
-
+.insuffiecient.modal {
+    background: white;
+    padding: 40px;
+}
+button.close {
+    position: absolute;
+    bottom: 19px;
+    right: 20px;
+}
 
 .popup-overlay.active{
 visibility:visible;
