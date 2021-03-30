9971a5c2f27564e9b7e2187d5f9d6c9b49bb4a4b
webdeveloper510 <developerweb6@gmail.com>
2021-03-30 23:54:21 +0530

    changes

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,7 +1,10 @@
 @include('layout.cdn')
+
+
+
+
 <link rel="stylesheet" href="{{asset('design/header.css')}}" />
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-
 
 
 
