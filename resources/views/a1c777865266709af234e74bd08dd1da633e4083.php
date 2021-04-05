3a2c51ba699662c06a11cca0932294159085b2e2
webdeveloper510 <developerweb6@gmail.com>
2021-04-06 05:05:52 +0530

    changes

--- resources/views/a1c777865266709af234e74bd08dd1da633e4083.php
+++ resources/views/a1c777865266709af234e74bd08dd1da633e4083.php
@@ -1306,7 +1306,7 @@
                     <a href="<?php echo e(url('artistDetail/'.$artist->artistid)); ?>">
                     <div class="row mb-3">
                       <div class="col">
-                        <img src="<?php echo e(url('storage/app/public/uploads/'.$artist->profilepicture)); ?>" class="img-fluid">
+                        <img src="<?php echo e($artist->profilepicture ? url('storage/app/public/uploads/'.$artist->profilepicture) : asset('images/profile-dummy.png')); ?>" class="img-fluid">
                         </div>
                                 <div class="col-6 mt-3">
                                 <p><?php echo e($artist->nickname); ?></p>
