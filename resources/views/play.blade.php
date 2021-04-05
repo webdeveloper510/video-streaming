bd6043b36a358cce1b4caf8fa7816a3f5caa107c
webdeveloper510 <developerweb6@gmail.com>
2021-04-06 01:22:55 +0530

    changes

--- resources/views/play.blade.php
+++ resources/views/play.blade.php
@@ -125,6 +125,8 @@
         @foreach($listname as $playlist)
         <?php 
               $videos = explode(',',$playlist->videos);
+
+              print_r($videos);
              
               $count = count($videos);
 
