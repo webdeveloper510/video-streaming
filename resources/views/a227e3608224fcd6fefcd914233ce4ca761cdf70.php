a69cecb29817d50b320690237ce390ecddc7265b
webdeveloper510 <developerweb6@gmail.com>
2021-04-08 16:01:58 +0000

    changes

--- resources/views/a227e3608224fcd6fefcd914233ce4ca761cdf70.php
+++ resources/views/a227e3608224fcd6fefcd914233ce4ca761cdf70.php
@@ -27,8 +27,7 @@ margin-top: 5%;background-color: #ebebeb;font-family: 'Roboto', sans-serif;">
                 <td style="padding-left: 15%;padding-right: 15%;">
                      <p>Hey <span><?php echo e($data['nickname']); ?></span>, Thank you for visiting our website. We are currently in development process.
                 We will notify you as soon as we are ready.
-               If you have any questions, concerns or other inquiries please reach out to us using:
-                                 <a href="mailto:contact@pornartistzone.com">contact@pornartistzone.com</a></p>
+              </p>
                 </td>
              </tr>
              <tr>
