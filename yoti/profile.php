389edf13ffa149967fa8df590d32a3d6c486bd04
webdeveloper510 <developerweb6@gmail.com>
2021-04-09 21:17:32 +0000

    changes

--- yoti/profile.php
+++ yoti/profile.php
@@ -1,18 +1,19 @@
 <?php
 use Yoti\Profile;
+use Yoti;
 try{
 
 include('./vendor/autoload.php');
-
-
 
 $oneTimeUseToken = $_GET['token'];
 
 $client = new \Yoti\YotiClient('3c6b5f75-5e38-44b3-b50d-33e8925adcae', './keys/verify.pem');
 
+print_r($client);die;
+
 $activityDetails = $client->getActivityDetails($oneTimeUseToken);
 
-print_r($activityDetails);die;
+//print_r($activityDetails);die;
 
 $applicationProfile = $activityDetails->getApplicationProfile();
 
