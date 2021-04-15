5cc7fe2fc2068c1f73099146e8c286fa62a3336c
webdeveloper510 <developerweb6@gmail.com>
2021-04-15 05:30:27 +0530

    changes

--- resources/views/artists/profile.blade.php
+++ resources/views/artists/profile.blade.php
@@ -66,11 +66,20 @@
    
       <div class="artistoffer row">
         <div class="col-md-2 mt-5">
+          @if($offer->type=='video')
         <video width="100%"  poster="{{url('storage/app/public/uploads/'.$offer->audio_pic) }}"   controlsList="nodownload" disablePictureInPicture>
                 <source src="{{url('storage/app/public/video/'.$offer->media) }}" type="video/mp4">
                 
                 Your browser does not support the video tag.
-            </video>
+         </video>
+         @else
+         <audio width="100%"  poster="{{url('storage/app/public/uploads/'.$offer->audio_pic) }}"   controlsList="nodownload" disablePictureInPicture>
+                <source src="{{url('storage/app/public/audio/'.$offer->media) }}" type="audio/mp3">
+                
+                Your browser does not support the video tag.
+         </audio>
+         @endif
+
       </div>
        
         <div class="col-md-8 pl-5 showoffer pt-5">
