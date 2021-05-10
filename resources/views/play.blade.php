2cdf5ddb597541b96ca0a3118d7dec0a8c906e92
unknown <developerweb6@gmail.com>
2021-05-11 02:25:30 +0530

    changes

--- resources/views/play.blade.php
+++ resources/views/play.blade.php
@@ -195,7 +195,7 @@
                     </div>
                                    <!-- Button trigger modal -->
                     <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
-                    Launch demo modal
+                    Content is no available
                     </button>
 
                     <!-- Modal -->
