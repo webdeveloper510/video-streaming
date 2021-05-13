bac8218f3b2a940b45fb84cddfcf433a4ce621bf
unknown <developerweb6@gmail.com>
2021-05-14 02:04:54 +0530

    changes

---
+++ resources/views/show_artist.blade.php
@@ -0,0 +1,189 @@
+@include('layouts.header')
+
+
+<section class="showartist">
+
+<div class="container">
+   <div class="row">
+     <div class="col-md-4">
+            <div class="card mt-5">
+            <img class="card-img-top" src="https://med.gov.bz/wp-content/uploads/2020/08/dummy-profile-pic-300x300.jpg" alt=" image cap">
+            <div class="card-body text-center">
+                <h3 class="card-title text-center">Artist Name <i class="fa fa-star" style="font-size:24px;color:red;"></i>999</h3>
+                <button class="btn btn-danger btn-lg" type="button"> Subscribe</button>
+                <hr>
+                <h5>Description</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.....</p>
+                <div class="row">
+                    <div class="col-6">
+                           <div class="">
+                               <h3>9 </h3>
+                               <h5>Followers</h3>
+                               </div>
+
+                           </div>
+                           <div class="col-6">
+                           <div class="">
+                               <h3>9 </h3>
+                               <h5>Collections</h3>
+                                 
+                           </div>
+                    </div>
+                </div>
+            </div>
+            </div>
+     </div>
+     <div class="col-md-4">
+            <div class="card mt-5">
+            <img class="card-img-top" src="https://med.gov.bz/wp-content/uploads/2020/08/dummy-profile-pic-300x300.jpg" alt=" image cap">
+            <div class="card-body text-center">
+                                <h3 class="card-title text-center">Artist Name <i class="fa fa-star" style="font-size:24px;color:red;"></i>999</h3>
+                <button class="btn btn-danger btn-lg" type="button"> Subscribe</button>
+                <hr>
+                <h5>Description</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.....</p>
+                <div class="row">
+                    <div class="col-6">
+                           <div class="">
+                               <h3>9 </h3>
+                               <h5>Followers</h3>
+                               </div>
+
+                           </div>
+                           <div class="col-6">
+                           <div class="">
+                               <h3>9 </h3>
+                               <h5>Collections</h3>
+                                 
+                           </div>
+                    </div>
+                </div>
+            </div>
+            </div>
+     </div>
+     <div class="col-md-4">
+            <div class="card mt-5">
+            <img class="card-img-top" src="https://med.gov.bz/wp-content/uploads/2020/08/dummy-profile-pic-300x300.jpg" alt=" image cap">
+            <div class="card-body text-center">
+                                <h3 class="card-title text-center">Artist Name <i class="fa fa-star" style="font-size:24px;color:red;"></i>999</h3>
+                <button class="btn btn-danger btn-lg" type="button"> Subscribe</button>
+                <hr>
+                <h5>Description</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.....</p>
+                <div class="row">
+                    <div class="col-6">
+                           <div class="">
+                               <h3>9 </h3>
+                               <h5>Followers</h3>
+                               </div>
+
+                           </div>
+                           <div class="col-6">
+                           <div class="">
+                               <h3>9 </h3>
+                               <h5>Collections</h3>
+                                 
+                           </div>
+                    </div>
+                </div>
+            </div>
+            </div>
+     </div>
+     <div class="col-md-4">
+            <div class="card mt-5">
+            <img class="card-img-top" src="https://med.gov.bz/wp-content/uploads/2020/08/dummy-profile-pic-300x300.jpg" alt=" image cap">
+            <div class="card-body text-center">
+                                <h3 class="card-title text-center">Artist Name <i class="fa fa-star" style="font-size:24px;color:red;"></i>999</h3>
+                <button class="btn btn-danger btn-lg" type="button"> Subscribe</button>
+                <hr>
+                <h5>Description</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.....</p>
+                <div class="row">
+                    <div class="col-6">
+                           <div class="">
+                               <h3>9 </h3>
+                               <h5>Followers</h3>
+                               </div>
+
+                           </div>
+                           <div class="col-6">
+                           <div class="">
+                               <h3>9 </h3>
+                               <h5>Collections</h3>
+                                 
+                           </div>
+                    </div>
+                </div>
+            </div>
+            </div>
+     </div>
+     <div class="col-md-4">
+            <div class="card mt-5">
+            <img class="card-img-top" src="https://med.gov.bz/wp-content/uploads/2020/08/dummy-profile-pic-300x300.jpg" alt=" image cap">
+            <div class="card-body text-center">
+                                <h3 class="card-title text-center">Artist Name <i class="fa fa-star" style="font-size:24px;color:red;"></i>999</h3>
+                <button class="btn btn-danger btn-lg" type="button"> Subscribe</button>
+                <hr>
+                <h5>Description</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.....</p>
+                <div class="row">
+                    <div class="col-6">
+                           <div class="">
+                               <h3>9 </h3>
+                               <h5>Followers</h3>
+                               </div>
+
+                           </div>
+                           <div class="col-6">
+                           <div class="">
+                               <h3>9 </h3>
+                               <h5>Collections</h3>
+                                 
+                           </div>
+                    </div>
+                </div>
+            </div>
+            </div>
+     </div>
+     <div class="col-md-4">
+            <div class="card mt-5">
+            <img class="card-img-top" src="https://med.gov.bz/wp-content/uploads/2020/08/dummy-profile-pic-300x300.jpg" alt=" image cap">
+            <div class="card-body text-center">
+                                <h3 class="card-title text-center">Artist Name <i class="fa fa-star" style="font-size:24px;color:red;"></i>999</h3>
+                <button class="btn btn-danger btn-lg" type="button"> Subscribe</button>
+                <hr>
+                <h5>Description</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.....</p>
+                <div class="row">
+                    <div class="col-6">
+                           <div class="">
+                               <h3>9 </h3>
+                               <h5>Followers</h3>
+                               </div>
+
+                           </div>
+                           <div class="col-6">
+                           <div class="">
+                               <h3>9 </h3>
+                               <h5>Collections</h3>
+                                 
+                           </div>
+                    </div>
+                </div>
+            </div>
+            </div>
+     </div>
+
+     
+     
+
+   </div>
+</div>
+</section>
+<style>
+section.showartist {
+    background: black;
+    padding-bottom: 40px;
+}
+</style>
+@include('layouts.footer')
