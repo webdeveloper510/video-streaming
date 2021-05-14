815871302a109a22137b7dbc6c6ebd265a5b42bb
unknown <developerweb6@gmail.com>
2021-05-15 00:40:31 +0530

    changes

--- resources/views/show_artist.blade.php
+++ resources/views/show_artist.blade.php
@@ -180,10 +180,304 @@
    </div>
 </div>
 </section>
+
+<section>
+
+<div class="row">
+    <div class="col-md-12">
+      <div class="carousel slide multi-item-carousel" id="theCarousel">
+        <div class="carousel-inner">
+          <div class="item active">
+            <div class="col-xs-4"><a href="#1">
+            <div class="card mt-5">
+            <img class="card-img-top" src="https://med.gov.bz/wp-content/uploads/2020/08/dummy-profile-pic-300x300.jpg" alt=" image cap">
+            <div class="card-body text-center">
+                                <h3 class="card-title text-center">Artist Name  <small style="    font-family: 'Poppins';"><i class="fa fa-star" style="color:red;"></i>999 </small></h3>
+                <button class="btn btn-danger btn-lg" type="button"> Subscribe</button>
+                <hr>
+                <h5>Description</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.....</p>
+                <div class="row">
+                    <div class="col-6">
+                           <div class="">
+                               <h3>9 </h3>
+                               <h5>Offer(S)</h3>
+                               </div>
+
+                           </div>
+                           <div class="col-6">
+                           <div class="">
+                               <h3>9 </h3>
+                               <h5>Collection </h3>
+                                 
+                           </div>
+                    </div>
+                </div>
+            </div>
+            </div>
+            </a>
+            </div>
+          </div>
+          <div class="item">
+            <div class="col-xs-4"><a href="#1">
+            
+            <div class="card mt-5">
+            <img class="card-img-top" src="https://med.gov.bz/wp-content/uploads/2020/08/dummy-profile-pic-300x300.jpg" alt=" image cap">
+            <div class="card-body text-center">
+                                <h3 class="card-title text-center">Artist Name  <small style="    font-family: 'Poppins';"><i class="fa fa-star" style="color:red;"></i>999 </small></h3>
+                <button class="btn btn-danger btn-lg" type="button"> Subscribe</button>
+                <hr>
+                <h5>Description</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.....</p>
+                <div class="row">
+                    <div class="col-6">
+                           <div class="">
+                               <h3>9 </h3>
+                               <h5>Offer(S)</h3>
+                               </div>
+
+                           </div>
+                           <div class="col-6">
+                           <div class="">
+                               <h3>9 </h3>
+                               <h5>Collection </h3>
+                                 
+                           </div>
+                    </div>
+                </div>
+            </div>
+            </div>
+            
+            </a></div>
+          </div>
+          <div class="item">
+            <div class="col-xs-4"><a href="#1">
+            
+            <div class="card mt-5">
+            <img class="card-img-top" src="https://med.gov.bz/wp-content/uploads/2020/08/dummy-profile-pic-300x300.jpg" alt=" image cap">
+            <div class="card-body text-center">
+                                <h3 class="card-title text-center">Artist Name  <small style="    font-family: 'Poppins';"><i class="fa fa-star" style="color:red;"></i>999 </small></h3>
+                <button class="btn btn-danger btn-lg" type="button"> Subscribe</button>
+                <hr>
+                <h5>Description</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.....</p>
+                <div class="row">
+                    <div class="col-6">
+                           <div class="">
+                               <h3>9 </h3>
+                               <h5>Offer(S)</h3>
+                               </div>
+
+                           </div>
+                           <div class="col-6">
+                           <div class="">
+                               <h3>9 </h3>
+                               <h5>Collection </h3>
+                                 
+                           </div>
+                    </div>
+                </div>
+            </div>
+            </div>
+            
+            
+            </a></div>
+          </div>
+          <div class="item">
+            <div class="col-xs-4"><a href="#1">
+            <div class="card mt-5">
+            <img class="card-img-top" src="https://med.gov.bz/wp-content/uploads/2020/08/dummy-profile-pic-300x300.jpg" alt=" image cap">
+            <div class="card-body text-center">
+                                <h3 class="card-title text-center">Artist Name  <small style="    font-family: 'Poppins';"><i class="fa fa-star" style="color:red;"></i>999 </small></h3>
+                <button class="btn btn-danger btn-lg" type="button"> Subscribe</button>
+                <hr>
+                <h5>Description</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.....</p>
+                <div class="row">
+                    <div class="col-6">
+                           <div class="">
+                               <h3>9 </h3>
+                               <h5>Offer(S)</h3>
+                               </div>
+
+                           </div>
+                           <div class="col-6">
+                           <div class="">
+                               <h3>9 </h3>
+                               <h5>Collection </h3>
+                                 
+                           </div>
+                    </div>
+                </div>
+            </div>
+            </div>
+            
+            </a></div>
+          </div>
+          <div class="item">
+            <div class="col-xs-4"><a href="#1">
+            <div class="card mt-5">
+            <img class="card-img-top" src="https://med.gov.bz/wp-content/uploads/2020/08/dummy-profile-pic-300x300.jpg" alt=" image cap">
+            <div class="card-body text-center">
+                                <h3 class="card-title text-center">Artist Name  <small style="    font-family: 'Poppins';"><i class="fa fa-star" style="color:red;"></i>999 </small></h3>
+                <button class="btn btn-danger btn-lg" type="button"> Subscribe</button>
+                <hr>
+                <h5>Description</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.....</p>
+                <div class="row">
+                    <div class="col-6">
+                           <div class="">
+                               <h3>9 </h3>
+                               <h5>Offer(S)</h3>
+                               </div>
+
+                           </div>
+                           <div class="col-6">
+                           <div class="">
+                               <h3>9 </h3>
+                               <h5>Collection </h3>
+                                 
+                           </div>
+                    </div>
+                </div>
+            </div>
+            </div>
+            
+            </a></div>
+          </div>
+          <div class="item">
+            <div class="col-xs-4"><a href="#1">
+            
+            <div class="card mt-5">
+            <img class="card-img-top" src="https://med.gov.bz/wp-content/uploads/2020/08/dummy-profile-pic-300x300.jpg" alt=" image cap">
+            <div class="card-body text-center">
+                                <h3 class="card-title text-center">Artist Name  <small style="    font-family: 'Poppins';"><i class="fa fa-star" style="color:red;"></i>999 </small></h3>
+                <button class="btn btn-danger btn-lg" type="button"> Subscribe</button>
+                <hr>
+                <h5>Description</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.....</p>
+                <div class="row">
+                    <div class="col-6">
+                           <div class="">
+                               <h3>9 </h3>
+                               <h5>Offer(S)</h3>
+                               </div>
+
+                           </div>
+                           <div class="col-6">
+                           <div class="">
+                               <h3>9 </h3>
+                               <h5>Collection </h3>
+                                 
+                           </div>
+                    </div>
+                </div>
+            </div>
+            </div>
+            
+            
+            
+            </a></div>
+          </div>
+          <!-- add  more items here -->
+          <!-- Example item start:  -->
+          
+          <div class="item">
+            <div class="col-xs-4"><a href="#1">
+            
+            <div class="card mt-5">
+            <img class="card-img-top" src="https://med.gov.bz/wp-content/uploads/2020/08/dummy-profile-pic-300x300.jpg" alt=" image cap">
+            <div class="card-body text-center">
+                                <h3 class="card-title text-center">Artist Name  <small style="    font-family: 'Poppins';"><i class="fa fa-star" style="color:red;"></i>999 </small></h3>
+                <button class="btn btn-danger btn-lg" type="button"> Subscribe</button>
+                <hr>
+                <h5>Description</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.....</p>
+                <div class="row">
+                    <div class="col-6">
+                           <div class="">
+                               <h3>9 </h3>
+                               <h5>Offer(S)</h3>
+                               </div>
+
+                           </div>
+                           <div class="col-6">
+                           <div class="">
+                               <h3>9 </h3>
+                               <h5>Collection </h3>
+                                 
+                           </div>
+                    </div>
+                </div>
+            </div>
+            </div>
+            
+            
+            
+            </a></div>
+          </div>
+          
+          <!--  Example item end -->
+        </div>
+        <a class="left carousel-control" href="#theCarousel" data-slide="prev"><i class="glyphicon glyphicon-chevron-left"></i></a>
+        <a class="right carousel-control" href="#theCarousel" data-slide="next"><i class="glyphicon glyphicon-chevron-right"></i></a>
+      </div>
+    </div>
+  </div>
+
+
+</section>
 <style>
 section.showartist {
     background: black;
     padding-bottom: 40px;
 }
+
+/* .carousel-inner{padding:20px;} */
+#theCarousel{padding:20px;}
+/*Issue happnes because of this ^^^ */
+.multi-item-carousel .carousel-inner > .item {
+  -webkit-transition: 500ms ease-in-out left;
+  transition: 500ms ease-in-out left;
+}
+.multi-item-carousel .carousel-inner .active.left {
+  left: -33%;
+}
+.multi-item-carousel .carousel-inner .active.right {
+  left: 33%;
+}
+.multi-item-carousel .carousel-inner .next {
+  left: 33%;
+}
+.multi-item-carousel .carousel-inner .prev {
+  left: -33%;
+}
 </style>
+
+
+
+<script>
+
+// Instantiate the Bootstrap carousel
+$('.multi-item-carousel').carousel({
+  interval: false
+});
+
+// for every slide in carousel, copy the next slide's item in the slide.
+// Do the same for the next, next item.
+$('.multi-item-carousel .item').each(function(){
+  var next = $(this).next();
+  if (!next.length) {
+    next = $(this).siblings(':first');
+  }
+  next.children(':first-child').clone().appendTo($(this));
+  
+  if (next.next().length>0) {
+    next.next().children(':first-child').clone().appendTo($(this));
+  } else {
+  	$(this).siblings(':first').children(':first-child').clone().appendTo($(this));
+  }
+});
+
+</script>
 @include('layouts.footer')
