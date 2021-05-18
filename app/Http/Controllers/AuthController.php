b2d3b8f1487052a4a9c3fb48cb57056153d2f851
Nikhil <amit@codenomad.net>
2021-05-17 22:13:25 -0700

    changes

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -1684,7 +1684,7 @@ public function readNotification(Request $request){
 
         $offerInfo = (array)$data;
         
-       // print_r($offerInfo);die;
+      //print_r($offerInfo);die;
 
 
           //print_r($offerInfo['id']);die;
