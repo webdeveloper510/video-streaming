b0b2fc0741037734e563b9856fd5661148c69354
Nikhil <amit@codenomad.net>
2021-05-18 21:58:21 -0700

    changes

--- resources/views/artists/provider.blade.php
+++ resources/views/artists/provider.blade.php
@@ -223,7 +223,7 @@ section.background1 {
 
        
 @include('artists.dashboard_footer')
-    <script src="//assets.transloadit.com/js/jquery.transloadit2-v3-latest.js"></script>
+    <!-- <script src="//assets.transloadit.com/js/jquery.transloadit2-v3-latest.js"></script>
     <link rel="stylesheet" href="https://releases.transloadit.com/uppy/robodog/v1.10.7/robodog.min.css">
 <script src="https://releases.transloadit.com/uppy/robodog/v1.10.7/robodog.min.js"></script>
   <script type="text/javascript">
@@ -400,4 +400,4 @@ window.Robodog.form('#myForm', {
             });
     }
 
-</script>
+</script> -->
