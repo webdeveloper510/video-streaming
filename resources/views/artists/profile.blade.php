efa0434ec16256cdcce2d4ad7e03580738ad63e9
Nikhil <amit@codenomad.net>
2021-05-28 00:37:13 -0700

    changes

--- resources/views/artists/profile.blade.php
+++ resources/views/artists/profile.blade.php
@@ -344,13 +344,13 @@ Your browser does not support the audio tag.
         <div class="col-md-8 col-sm-8 col-lg-8">
        
           @if(isset($random[0]->type)&&$random[0]->type=='video')
-            <video width="100%" height="100%" id="get_duration"  poster="{{url('storage/app/public/uploads/'.$random[0]->audio_pic) }}" controlsList="nodownload" disablePictureInPicture>
+            <video width="100%" height="100%" id="get_duration"  poster="{{url('storage/app/public/uploads/'.$random[0]->audio_pic) }}" controls List="nodownload" disablePictureInPicture>
                       <source src="{{isset($random[0]->media) ? url('storage/app/public/video/'.$random[0]->media) :'https://www.radiantmediaplayer.com/media/big-buck-bunny-360p.mp4' }}" type="video/mp4">
                       Your browser does not support the video tag.
           </video>
           @else
           <img src="{{isset($random[0]->audio_pic) ? url('storage/app/public/uploads/'.$random[0]->audio_pic) : 'https://static.vecteezy.com/system/resources/thumbnails/000/097/181/small/free-black-leather-vector.jpg'}}" width="100%;">
-          <audio  poster="{{url('storage/app/public/uploads/'.$random->audio_pic) }}" width="100%" height="100%" id="get_duration"  controlsList="nodownload" disablePictureInPicture>
+          <audio  poster="{{url('storage/app/public/uploads/'.$random->audio_pic) }}" width="100%" height="100%" id="get_duration"  controls List="nodownload" disablePictureInPicture>
                <source src="{{isset($random[0]->media) ? url('storage/app/public/audio/'.$random[0]->media) :'' }}" type="audio/mp3">          
           </audio>
           @endif
