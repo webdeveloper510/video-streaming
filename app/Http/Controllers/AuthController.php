2859e8ba11e19dd61ded9ac1ff47d816ffc227af
Nikhil <amit@codenomad.net>
2021-06-05 00:26:47 -0700

    changes

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -637,8 +637,18 @@ else{
 
 
 
-    public function logout(Request $request){
-      Session::forget('User');
+    public function logout($text=null){
+      if($text=='default'){
+
+        Session::forget('pazLogin');
+
+
+      }
+
+      else{
+        Session::forget('User');
+      }
+    
        Session::flush();
 
        return redirect('/');
