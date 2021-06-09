f51405ce1f9374d97c3826c40110858592b93565
webdeveloper510 <developerweb6@gmail.com>
2021-06-09 17:19:12 +0000

    changes

--- resources/views/9ed2611e1e6fb253c3aee1f60e438e950ea03d5b.php
+++ resources/views/9ed2611e1e6fb253c3aee1f60e438e950ea03d5b.php
@@ -74,7 +74,12 @@
 
   </div>
 </section>
-<div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
+<div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+<ol class="carousel-indicators">
+    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+  </ol>
   <div class="carousel-inner">
     <div class="carousel-item active">
     <div class="row media">
@@ -155,7 +160,20 @@
        </div>
     </div>
   </div>
+  <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
 </div>
+
+
+
+
+
   
   
   </div>
