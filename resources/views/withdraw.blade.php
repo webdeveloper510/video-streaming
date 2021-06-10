28f8eed964f04030ea173828c022f71b294467eb
unknown <developerweb6@gmail.com>
2021-06-10 23:11:41 +0530

    changes

--- resources/views/withdraw.blade.php
+++ resources/views/withdraw.blade.php
@@ -209,7 +209,7 @@
             <div class="form-group row">
               <label for="staticEmail" class="col-sm-6 col-form-label">Paxum Account</label>
               <div class="col-sm-6">
-                <button class="btn btn-outline-primary" type="button">Enter payout <br> Information</button>
+                <button class="btn btn-outline-primary"data-toggle="modal" data-target="#panxum" type="button">Enter payout <br> Information</button>
               </div>
             </div>
           </form>
@@ -228,6 +228,96 @@
     </div>
   </div>
 </div>
+
+
+       <!-- Paxun Account popup -->
+
+       
+
+        <!-- Modal -->
+        <div class="modal fade" id="panxum" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+          <div class="modal-dialog" role="document">
+            <div class="modal-content">
+              <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel"> Paxum Account</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                  <span aria-hidden="true">&times;</span>
+                </button>
+              </div>
+              <div class="modal-body">
+              <form>
+                  <div class="form-group">
+                    <label for="exampleInputEmail1">Email </label>
+                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+                  </div>
+                  <div class="form-group">
+                    <label for="exampleInputPassword1">Refrence</label>
+                    <input type="text" class="form-control" id="exampleInputPassword1">
+                  </div>
+                  <div class="text-center">
+                  <button type="submit" class="btn btn-primary">Save</button>
+                  </div>
+                </form>
+              </div>
+           
+            </div>
+          </div>
+        </div>
+
+
+<!-- Bank Account popup -->
+
+            
+
+              <!-- Modal -->
+              <div class="modal fade" id="bank" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div class="modal-dialog" role="document">
+                  <div class="modal-content">
+                    <div class="modal-header">
+                      <h5 class="modal-title" id="exampleModalLabel">Bank Account</h5>
+                      <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                      </button>
+                    </div>
+                    <div class="modal-body">
+                    <form>
+                      <div class="form-group">
+                        <label >Email address</label>
+                        <input type="text" class="form-control" placeholder="Enter ">
+                      </div>
+                      <div class="form-group">
+                        <label >Email address</label>
+                        <input type="text" class="form-control" placeholder="Enter ">
+                      </div>
+                      <div class="form-group">
+                        <label >Email address</label>
+                        <input type="text" class="form-control" placeholder="Enter ">
+                      </div>
+                      <div class="form-group">
+                        <label >Bank Name </label>
+                        <input type="text" class="form-control" placeholder="Enter ">
+                      </div>
+                      <div class="form-group">
+                        <label >Bank address</label>
+                        <input type="text" class="form-control" placeholder="Enter ">
+                      </div>
+
+                      <div class="form-group">
+                        <label for="exampleInputPassword1">BIC</label>
+                        <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+                      </div>
+                      <div class="form-check">
+                        <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                        <label class="form-check-label" for="exampleCheck1">Check me out</label>
+                      </div>
+                      <button type="submit" class="btn btn-primary">Submit</button>
+                    </form>
+                    </div>
+                    
+                  </div>
+                </div>
+              </div>
+
       <!-- End Navbar -->
      <style>
   .background1{
