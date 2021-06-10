273e697bd755a3073f3a1f792a03da95b80b4cc4
unknown <developerweb6@gmail.com>
2021-06-11 02:33:36 +0530

    changes

--- resources/views/withdraw.blade.php
+++ resources/views/withdraw.blade.php
@@ -235,7 +235,7 @@
        
 
         <!-- Modal -->
-        <div class="modal fade" id="panxum" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal fade mt-5" id="panxum" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
           <div class="modal-dialog" role="document">
             <div class="modal-content">
               <div class="modal-header">
