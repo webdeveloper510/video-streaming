db446e3d18fa28f129c50f7296d7e564b0850ba4
Nikhil <amit@codenomad.net>
2021-06-10 03:26:40 -0700

    changes

--- resources/views/artists/profile.blade.php
+++ resources/views/artists/profile.blade.php
@@ -240,9 +240,9 @@
    <div class="checkall" style="display:none"><form> 
    <input type="checkbox" class="slct_video"></form></div>
      <a href="{{url('artistVideo/'.$aud->id)}}">
-    <img src="{{$aud->audio_pic ?  url('storage/app/public/uploads/'.$aud->audio_pic) : asset('images/logos/voice.jpg')}}" width="100%">
+    <!-- <img src="{{$aud->audio_pic ?  url('storage/app/public/uploads/'.$aud->audio_pic) : asset('images/logos/voice.jpg')}}" width="100%"> -->
 
-<video controls controlsList="nodownload" id="audio_{{$aud->id}}"   poster="{{url('storage/app/public/uploads/'.$aud->audio_pic) }}" disablePictureInPicture>
+<video width="100%" controls controlsList="nodownload" id="audio_{{$aud->id}}" poster="{{url('storage/app/public/uploads/'.$aud->audio_pic) }}" disablePictureInPicture>
 
 <source src="{{url('storage/app/public/video/'.$aud->media) }}" type="video/mp4">
 Your browser does not support the audio tag.
