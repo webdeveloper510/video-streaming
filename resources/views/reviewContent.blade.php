b5805310ae6ba33298f938ccfb8c6e1ec3e7163c
Nikhil <amit@codenomad.net>
2021-06-17 22:01:52 -0700

    changes

--- resources/views/reviewContent.blade.php
+++ resources/views/reviewContent.blade.php
@@ -10,11 +10,10 @@
   <div class="col-md-3">
      <div class="row">
         <div class="col-4">
-         
-<video width="100%" height="100%" controls>
-  <source src="movie.mp4" type="video/mp4">
-  <source src="movie.ogg" type="video/ogg">
-</video>
+        <video width="100%" height="100%" controls>
+          <source src="movie.mp4" type="video/mp4">
+          <source src="movie.ogg" type="video/ogg">
+        </video>
         </div>
         <div class="col-8">
          <h5> Video title</h5>
