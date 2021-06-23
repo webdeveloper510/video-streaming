d0f1040c136c492917ec17c1aa140e0bd970d5f1
Nikhil <amit@codenomad.net>
2021-06-23 01:57:56 -0700

    changes

--- resources/views/artistoffers.blade.php
+++ resources/views/artistoffers.blade.php
@@ -78,8 +78,8 @@
         <input type="hidden" name="user_id" value="{{$GLOBALS['id'].'_'.$GLOBALS['user_id']}}"/>
         <input type="hidden" name="price" id="offer_pay" value="{{$offerdata->max*$GLOBALS['price']}}"/>
         <input type="hidden" name="timezone" class="timezone" value="{{$offerdata->timezone}}"/>
-        <input type="hidden" name="artist_time_at" class="created_at" value=""/>
-        <input type="hidden" name="artist_updated_at" class="updated_at" value="{{$offerdata->category}}"/>
+        <input type="hidden" name="created_at" class="artist_time_at" value=""/>
+        <input type="hidden" name="updated_at" class="artist_updated_at" value="{{$offerdata->category}}"/>
 
         <input type="hidden" name="art_id" value="{{$GLOBALS['artistid']}}">
          
