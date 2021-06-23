b62d3d6da6f53e989a67f7b201605136d8e40901
Nikhil <amit@codenomad.net>
2021-06-23 02:42:26 -0700

    changes

--- resources/views/artistoffers.blade.php
+++ resources/views/artistoffers.blade.php
@@ -146,7 +146,7 @@
 
         <script>
     var today = new Date();
-    consoe.log(today);
+    console.log(today);
     var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
     var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
     var dateTime = date+' '+time;
