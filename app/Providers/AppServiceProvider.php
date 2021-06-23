a0b2a643ff3f2692dd05c548960add84f63fe1fa
Nikhil <amit@codenomad.net>
2021-06-22 22:21:03 -0700

    changes

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -48,8 +48,8 @@ class AppServiceProvider extends ServiceProvider
 
                $count = $array['count'];
 
-               $today = new DateTime("now", new DateTimeZone('America/New_York') );
-               echo $today->format('Y-m-d');
+              //  $today = new DateTime("now", new DateTimeZone('America/New_York') );
+              //  echo $today->format('Y-m-d');
 
                $data=Session::get('User');
                
