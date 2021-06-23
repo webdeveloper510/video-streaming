6f807cb66b72e6e7d2dafe524b70627a4011719a
Nikhil <amit@codenomad.net>
2021-06-22 23:36:49 -0700

    CHANGES

--- public/js/my.js
+++ public/js/my.js
@@ -2772,7 +2772,7 @@ function time(){
     var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
     var dateTime = date+' '+time;
 
-var  date = new Date ( dateTime + "+HH:MM" );
+var  date = new Date ( dateTime );
 console.log("UTC TIME", date)
 console.log("LOCAL TIME", date.toLocaleString() );
     // var date = new Date('2021-06-21 19:26:57');
