cd67878e3cdb61f1a7ee1503da775c8c138706db
lucky282 <66032866+lucky282@users.noreply.github.com>
2021-06-26 02:43:41 +0530

    changes

--- resources/views/artistprofilepage.blade.php
+++ resources/views/artistprofilepage.blade.php
@@ -11,7 +11,7 @@
           <video width="100%" height="100%" controls>
                     <source src="movie.mp4" type="video/mp4">
                     <source src="movie.ogg" type="video/ogg">
-                    Your browser does not support the video tag.
+                    
                 </video>
 
         </div>
