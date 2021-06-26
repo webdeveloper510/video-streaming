50599d9edb59d190208ad3cd1f2e431c171ff261
Nikhil <amit@codenomad.net>
2021-06-26 20:11:14 +0530

    changes

--- public/js/my.js
+++ public/js/my.js
@@ -512,9 +512,15 @@ $(document).on('click', '.add_price', function () {
 
 })
 
-$(window).on('popstate', function(event) {
-    alert("pop");
-   });
+$(function() {
+    if (window.history && window.history.pushState) {
+        window.history.pushState('', null, './');
+        $(window).on('popstate', function() {
+            alert('Back button was pressed.');
+           // document.location.href = '/submit';
+        });
+    }
+    )};
 $(document).on('click', '.additional_price', function () {
     alert('hello');
 })
