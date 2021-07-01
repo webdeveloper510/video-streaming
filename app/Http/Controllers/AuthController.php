2d8b3b7f17a5a9fb4aeae781d93791c2ce0bc8ed
Nikhil <amit@codenomad.net>
2021-07-01 23:02:12 +0530

    chages

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -891,7 +891,7 @@ else{
       return response()->json(['errors'=>$validator->errors()->all()]);
   }
 
-  print_r($request->all());die;
+ // print_r($request->all());die;
       if($request->radio=='video'){
             $data=$request->all();
               $fileName = time().'_'.$request->media->getClientOriginalName();
