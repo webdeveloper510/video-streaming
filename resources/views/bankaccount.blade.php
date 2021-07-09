6d2ad644d044c3b6bcd3df73d8de9adaecac5d30
lucky282 <66032866+lucky282@users.noreply.github.com>
2021-07-09 20:43:38 +0530

    changes

--- resources/views/bankaccount.blade.php
+++ resources/views/bankaccount.blade.php
@@ -5,7 +5,7 @@
       <div class="float-right">
          <a href="{{url('/logout/default')}}"><button class="btn btn-primery">Logout</button></a>
       </div>
-      <h1 class="text-white mt-2"> Content Review</h1>
+      <h1 class="text-white mt-2"> Bank Account Information</h1>
    </div>
 </header>
 
@@ -16,7 +16,7 @@
               <div class="col-md-6">
                 <div class="card">
                 <div class="card-header">
-                <h3> Paxum Account</h3>
+                <h3> Bank Account</h3>
                 </div>
                 <div class="card-body">
                 <form>
