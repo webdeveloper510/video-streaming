96a4f1f37799ddacfb493ac8ebe4f3b45d21e8d9
lucky282 <66032866+lucky282@users.noreply.github.com>
2021-07-14 03:13:46 +0530

    changes

--- resources/views/reviewContent.blade.php
+++ resources/views/reviewContent.blade.php
@@ -147,10 +147,6 @@
 </form>
 
       </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Save changes</button>
-      </div>
     </div>
   </div>
 </div>
