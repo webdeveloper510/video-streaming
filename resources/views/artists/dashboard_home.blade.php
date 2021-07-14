af993912bc300d260416636f714e6abd9d043080
lucky282 <66032866+lucky282@users.noreply.github.com>
2021-07-14 23:37:26 +0530

    changes

--- resources/views/artists/dashboard_home.blade.php
+++ resources/views/artists/dashboard_home.blade.php
@@ -772,18 +772,15 @@
                                     <tr>
                                        <th scope="row">1</th>
                                        <td><input type="text" class="form-control"><br>
-                                       
+                                       <div class="custom-file">
+                                          <input type="file" class="custom-file-input" id="inputGroupFile01">
+                                          <label class="custom-file-label form-control" for="inputGroupFile01">Choose file</label>
+                                       </div>
                                        </td>
                                        <td> 
                                          <input type="date" class="form-control">
                                        </td>
                                     </tr>
-                                    <tr>
-                                       <td colspan="3"><div class="custom-file">
-                                          <input type="file" class="custom-file-input" id="inputGroupFile01">
-                                          <label class="custom-file-label form-control" for="inputGroupFile01">Choose file</label>
-                                       </div></td>
-</tr>
                                    
                                  </tbody>
                               </table>
