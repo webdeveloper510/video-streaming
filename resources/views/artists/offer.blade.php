5325f71e53696bc48e720b16d7ab2740a187cef5
lucky282 <66032866+lucky282@users.noreply.github.com>
2021-07-17 01:28:44 +0530

    chnages

--- resources/views/artists/offer.blade.php
+++ resources/views/artists/offer.blade.php
@@ -29,9 +29,21 @@
      
         
           <div class="row align-items-center text-white">
-           <div class="col-md-6">
+          <div class="col-md-12">
+            <div class="form-group row">
+              <label for="staticEmail" class="col-sm-5 col-form-label">Are there Co-Performers involved in this Content?</label>
+              <div class="col-sm-7">
+              <div class="radiobtn text-white">
+          <input type="radio"  name="type" value="Yes" /><p class="text-white">yes</p>
+          <input type="radio"class="ml-5" name="type" value="No"/><p class="text-white">No</p>
+
+            </div>
+              </div>
+            </div>
+            </div>
+          <div class="col-md-6">
            <div class="form-group">
-    <label >Are there Co-Performers involved in this Content?</label>
+ 
     <select class="custom-select selctc&r" id="inputGroupSelect01">
     <option selected>Choose...</option>
     <option value="1">Nicknames</option>
@@ -40,13 +52,7 @@
   </div>
 </div>
   <div class="col-md-6">
-  <div class="radiobtn text-white">
-          <input type="radio"  name="type" value="Yes" /><p class="text-white">yes</p>
-          <input type="radio"class="ml-5" name="type" value="No"/><p class="text-white">No</p>
-
-            </div>
-            <br>
-          <button class="btn btn-secondery selctc&r " type="button">+</button>
+            <button class="btn btn-secondery selctc&r " type="button">+</button>
           </div>
                     <div class="col-md-4 mt-5 ">
 
